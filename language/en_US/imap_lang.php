14972e0d3284387ead8ab44f4d4959239dd60400
Ben Chambers <bchambers@clearfoundation.com>
2012-06-28 19:13:33 +0000

    - Added description

--- language/en_US/imap_lang.php
+++ language/en_US/imap_lang.php
@@ -1,8 +1,7 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP and POP Server';
+$lang['imap_app_description'] = 'The IMAP and POP server provides a standard set of services for mail clients (Thunderbird, Mail, Evolution, Outlook/Express etc.) to connect to the server and retrieve/display email.';
 $lang['imap_app_name'] = 'IMAP and POP Server';
-
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'Secure IMAP';
 $lang['imap_pop3'] = 'POP';

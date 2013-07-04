b22137ed7baf7e39b53539cc935636486c651bf7
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-07-04 18:18:34 +0000

    - Prepped for errata update

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'imap';
-$app['version'] = '1.4.20';
+$app['version'] = '1.4.40';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';

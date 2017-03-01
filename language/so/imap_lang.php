bdb701f98143a1e8ed3cdf33422ff692b8317ba6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:43:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'server IMAP iyo POP waxay bixisaa go&#39;an caadiga ah ee adeegyada macaamiisha mail (Thunderbird, Mail, Evolution, Muuqaalka / Express iwm) in ay ku xidhmaan server iyo dib u soo ceshano / email bandhigay.';
+$lang['imap_app_name'] = 'IMAP iyo POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail fashilmay magacyo';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP Secure';
+$lang['imap_mail_service_invalid'] = 'adeegga Mail waa baadil.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP Secure';
+$lang['imap_push_email'] = 'Ka murugeeysan dib E-mail';

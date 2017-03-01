6d901be28188c43b0f0f03d08b70254b9afa14ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:42:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'මෙම IMAP හා POP සේවාදායකය සේවාදායකය වෙත සම්බන්ධ වීමට සහ / සංදර්ශකය ඊ-තැපැල් ලබා ගැනීමට තැපැල් ගනුදෙනුකරුවන් සඳහා සේවා සම්මත කට්ටලයක් (තණ්ඩර්බර්ඩ්, මේල්, පරිණාමය, ඉදිරි දැක්මක් / එක්ස්ප්රස් ආදිය) සපයයි.';
+$lang['imap_app_name'] = 'IMAP හා POP සේවාදායකය';
+$lang['imap_failed_logins'] = 'IMAP තැපැල් හමුවිය අසාර්ථක විය';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'ආරක්ෂිත IMAP';
+$lang['imap_mail_service_invalid'] = 'තැපැල් සේවා වලංගු නැත.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'ආරක්ෂිත POP';
+$lang['imap_push_email'] = 'තල්ලුව ඊ-තැපැල';

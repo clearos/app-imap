c34db34e46ad31572e9491b872a8f3ce90a6a610
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:45:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Awọn IMAP ati POP server pese a boṣewa ṣeto awọn iṣẹ fun mail ibara (Thunderbird, Mail, Evolution, Outlook / Express ati be be lo) lati sopọ si olupin ati gba / àpapọ imeeli.';
+$lang['imap_app_name'] = 'IMAP ati POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail Kuna logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail iṣẹ ni invalid.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Titari E-mail';

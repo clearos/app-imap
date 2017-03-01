ab208a52386dc461051c2732a9505aa9ad9e6828
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:29:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = '的IMAP和POP服务器提供了一组标准的邮件客户端（雷鸟邮件，演变的Outlook /快递等）的服务，以连接到该服务器并检索/显示的电子邮件地址。';
+$lang['imap_app_name'] = 'IMAP和POP服务器';
+$lang['imap_failed_logins'] = 'IMAP邮件登录失败';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = '安全IMAP';
+$lang['imap_mail_service_invalid'] = '邮件服务是无效的。';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = '安全POP';
+$lang['imap_push_email'] = '推送邮件';

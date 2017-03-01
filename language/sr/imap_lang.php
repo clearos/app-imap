181f00becb228be8c8e47e14bf0727e051ccbbfd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:42:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'ИМАП и ПОП сервер обезбеђује стандардни сет услуга за клијенте поште (Тхундербирд, Маил, Еволутион, Оутлоок / Екпресс и сл) да се повеже са сервером и преузели / Дисплаи емаил.';
+$lang['imap_app_name'] = 'ИМАП и ПОП сервер';
+$lang['imap_failed_logins'] = 'ИМАП маил неуспешних приступа';
+$lang['imap_imap'] = 'ИМАП';
+$lang['imap_imaps'] = 'secure ИМАП';
+$lang['imap_mail_service_invalid'] = 'Маил сервис је неважећи.';
+$lang['imap_pop3'] = 'ПОП';
+$lang['imap_pop3s'] = 'сецуре ПОП';
+$lang['imap_push_email'] = 'Пусх е-маил';

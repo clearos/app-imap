983ad3e16604c9542a12461b4641bc0d2dd5bbe9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:41:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'यो IMAP र POP सर्भरले मेल ग्राहकहरु (थन्डरबर्ड, मेल, विकास, आउटलुक / एक्सप्रेस आदि) को लागि सेवा को एक मानक सेट सर्भर जडान र पुन: प्राप्त गर्न / प्रदर्शन इमेल गर्न प्रदान गर्दछ।';
+$lang['imap_app_name'] = 'IMAP र POP सर्भर';
+$lang['imap_failed_logins'] = 'IMAP मेल लगइनहरू असफल';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'सुरक्षित IMAP';
+$lang['imap_mail_service_invalid'] = 'मेल सेवा अमान्य छ।';
+$lang['imap_pop3'] = 'पप';
+$lang['imap_pop3s'] = 'सुरक्षित POP';
+$lang['imap_push_email'] = 'पुस ई-मेल';

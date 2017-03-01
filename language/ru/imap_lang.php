9ef424ad1a15a6af916800852a4ee759f7650086
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:42:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Сервер IMAP и POP предоставляет стандартный набор услуг для почтовых клиентов (Thunderbird, Mail, Evolution, Outlook / Express и т.п.), чтобы подключиться к серверу и получать электронную почту / дисплей.';
+$lang['imap_app_name'] = 'IMAP и POP-сервер';
+$lang['imap_failed_logins'] = 'IMAP почты неудачных попыток входа';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Безопасный IMAP';
+$lang['imap_mail_service_invalid'] = 'Почтовая служба является недействительным.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Безопасный POP';
+$lang['imap_push_email'] = 'Нажмите Электронная почта';

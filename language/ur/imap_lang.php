5ee84162dd6a80a8098d38ae795d95c32ad5dffa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:44:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP اور POP سرور سرور سے رابطہ قائم کریں اور بازیافت / ڈسپلے ای میل پر میل کلائنٹس (تھنڈر برڈ، میل، ارتقاء، آؤٹ لک / ایکسپریس وغیرہ) کے لیے خدمات کا ایک معیاری سیٹ فراہم.';
+$lang['imap_app_name'] = 'IMAP اور POP سرور';
+$lang['imap_failed_logins'] = 'IMAP میل لاگ ان ناکام ہوگیا';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'محفوظ IMAP';
+$lang['imap_mail_service_invalid'] = 'میل سروس غلط ہے.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'محفوظ POP';
+$lang['imap_push_email'] = 'پش ای میل';

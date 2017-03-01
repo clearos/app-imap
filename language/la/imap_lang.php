37d6529524580a2cc5fb23617ad3b9eec6298e0c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:39:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'POP signum praebet et in IMAP ex meritis pro mail clients (Thunderbird, Mail, Evolution, Outlook / etc Express), ad coniungere ut retrieve et Server / display email.';
+$lang['imap_app_name'] = 'Et IMAP POP';
+$lang['imap_failed_logins'] = 'Mail IMAP robur Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail Service est invalidum.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'E-mail ventilabis';

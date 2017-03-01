7d363988e714587d2e8960e56d232b811f40dccd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:45:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Các IMAP và POP server cung cấp một bộ tiêu chuẩn của dịch vụ cho các khách hàng mail (Thunderbird, Mail, Evolution, Outlook / tốc vv) để kết nối đến máy chủ và lấy / hiển thị email.';
+$lang['imap_app_name'] = 'IMAP và POP server';
+$lang['imap_failed_logins'] = 'IMAP Thư Không Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP an toàn';
+$lang['imap_mail_service_invalid'] = 'dịch vụ email không hợp lệ.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP an toàn';
+$lang['imap_push_email'] = 'Push E-mail';

8dd4f775ce6f58ace9c6a331c6e5dede9faa7070
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:22:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP i POP server pruža standardni set usluga za mail klijenata (Thunderbird, Mail, Evolution, Outlook / Express, itd) za povezivanje sa serverom i dohvatiti / prikaz e-pošte.';
+$lang['imap_app_name'] = 'IMAP i POP servera';
+$lang['imap_failed_logins'] = 'IMAP Mail neuspješni upisi';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Pošte je nevažeća.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Push E-mail';

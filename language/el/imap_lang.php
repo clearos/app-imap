df4eda1c9b46a650253f70fd5d0a74655b2bfc77
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:36:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Ο διακομιστής IMAP και POP παρέχει ένα τυποποιημένο σύνολο των υπηρεσιών για τους πελάτες ηλεκτρονικού ταχυδρομείου (Thunderbird, Mail, Evolution, Outlook / Express, κλπ) για να συνδεθείτε στο διακομιστή και να ανακτήσετε / email οθόνη.';
+$lang['imap_app_name'] = 'IMAP και POP διακομιστή';
+$lang['imap_failed_logins'] = 'IMAP Mail Αποτυχία Συνδέσεις';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'ασφαλής IMAP';
+$lang['imap_mail_service_invalid'] = 'υπηρεσία ηλεκτρονικού ταχυδρομείου είναι έγκυρη.';
+$lang['imap_pop3'] = 'ΚΡΟΤΟΣ';
+$lang['imap_pop3s'] = 'ασφαλής POP';
+$lang['imap_push_email'] = 'Push E-mail';

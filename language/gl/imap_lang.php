f34d1c841060e14903d267f44fdc4f3b7f9ca3c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:36:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'O servidor IMAP e POP ofrece un conxunto estándar de servizos para clientes de correo (Thunderbird, Mail, Evolution, Outlook / Expreso etc.) para conectarse ao servidor e recuperar correo electrónico / display.';
+$lang['imap_app_name'] = 'IMAP e POP Servidor';
+$lang['imap_failed_logins'] = 'IMAP correo Erro logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'seguro IMAP';
+$lang['imap_mail_service_invalid'] = 'servizo de correo electrónico non é válido.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'seguro POP';
+$lang['imap_push_email'] = 'Push email';

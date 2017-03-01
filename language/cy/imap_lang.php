7e35163951fe0f2740dedfff4afcb87b95586faf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:45:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Mae&#39;r gweinydd IMAP a POP yn darparu set safonol o wasanaethau i gleientiaid bost (Thunderbird, Mail, Evolution, Outlook / Express ac ati) er mwyn cysylltu â&#39;r gweinydd ac adfer / e-bost arddangos.';
+$lang['imap_app_name'] = 'IMAP a POP Gweinydd';
+$lang['imap_failed_logins'] = 'Methwyd IMAP Mail mewngofnodi';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP diogel';
+$lang['imap_mail_service_invalid'] = 'gwasanaeth post yn annilys.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP diogel';
+$lang['imap_push_email'] = 'Gwthiwch E-bost';

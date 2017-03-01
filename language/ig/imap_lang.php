2724de157f955efeb1170e34f81794b149af4cbf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:38:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'The IMAP na Pop nkesa na-enye a ọkọlọtọ set nke ọrụ maka mail ahịa (Thunderbird, Mail, Evolution, Outlook / Express wdg) jikọọ na ihe nkesa na weghachite / ngosi email.';
+$lang['imap_app_name'] = 'IMAP na Pop Server';
+$lang['imap_failed_logins'] = 'IMAP Mail Okpu logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail ozi bụ ghara ịdị irè.';
+$lang['imap_pop3'] = 'Pop';
+$lang['imap_pop3s'] = 'Secure Pop';
+$lang['imap_push_email'] = 'Push E-mail';

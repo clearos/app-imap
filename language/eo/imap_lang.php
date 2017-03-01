91320b9b8753f21601b64b1f6014403a4185adc0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:36:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'La IMAP kaj POPO servilo disponigas norman aron de servoj por klientoj de poŝto (Thunderbird, Poŝto, Evolution, Outlook / Express ktp) por konekti al la servilo kaj elsxuti / montriĝo retpoŝto.';
+$lang['imap_app_name'] = 'IMAP kaj POPO Servilo';
+$lang['imap_failed_logins'] = 'IMAP Poŝto Malsukcesaj logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'sekura IMAP';
+$lang['imap_mail_service_invalid'] = 'Retpoŝtservon estas nevalida.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'sekura POP';
+$lang['imap_push_email'] = 'Puŝo Retpoŝto';

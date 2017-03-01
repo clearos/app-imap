cd9159c07adc14758c779b17db61246593718725
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:44:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP మరియు POP సర్వర్ సర్వర్ కనెక్ట్ అయ్యేందుకు మరియు / ప్రదర్శన ఇమెయిల్ తిరిగి పొందడానికి మెయిల్ క్లయింట్లు (థండర్బర్డ్, మెయిల్, ఎవల్యూషన్ ఔట్లుక్ / ఎక్స్ప్రెస్ మొదలైనవి) కోసం సేవల ప్రామాణిక అమరుస్తుంది.';
+$lang['imap_app_name'] = 'IMAP మరియు POP సర్వర్';
+$lang['imap_failed_logins'] = 'IMAP మెయిల్ విఫలమైంది లాగిన్';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'సురక్షిత IMAP';
+$lang['imap_mail_service_invalid'] = 'మెయిల్ సేవ చెల్లదు.';
+$lang['imap_pop3'] = 'పాప్';
+$lang['imap_pop3s'] = 'సురక్షిత POP';
+$lang['imap_push_email'] = 'పుష్ ఇ-మెయిల్';

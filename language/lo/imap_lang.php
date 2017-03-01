1e12352c673ca0add17deba1a2f724ee76369c81
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:39:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'ເຄື່ອງແມ່ຂ່າຍຂອງ IMAP ແລະ POP ໃຫ້ຊຸດມາດຕະຖານຂອງການບໍລິການສໍາລັບລູກຄ້າ mail (Thunderbird, Mail, Evolution, ການຄາດຄະເນ / ການສະແດງອອກແລະອື່ນໆ) ເພື່ອເຊື່ອມຕໍ່ກັບເຄື່ອງແມ່ຂ່າຍແລະດຶງ email / ການສະແດງ.';
+$lang['imap_app_name'] = 'IMAP ແລະ POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail ບໍ່ສາມາດເຂົ້າສູ່ລະບົບ';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP ທີ່ປອດໄພ';
+$lang['imap_mail_service_invalid'] = 'ການບໍລິການ Mail ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP ທີ່ປອດໄພ';
+$lang['imap_push_email'] = 'Push E-mail';

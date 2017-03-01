40b68290e514ce7132367c07003fde1b16cbe1d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:41:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Serwer IMAP i POP zapewnia standardowy zestaw usług dla klientów pocztowych (Thunderbird, Evolution, Mail, Outlook / Express, etc.), aby połączyć się z serwerem i pobrać / wyświetlacz e-mail.';
+$lang['imap_app_name'] = 'IMAP i POP Server';
+$lang['imap_failed_logins'] = 'IMAP Poczta Failed Logowania';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Bezpieczne IMAP';
+$lang['imap_mail_service_invalid'] = 'poczta jest nieprawidłowy.';
+$lang['imap_pop3'] = 'MUZYKA POP';
+$lang['imap_pop3s'] = 'Bezpieczne POP';
+$lang['imap_push_email'] = 'Push e-mail';

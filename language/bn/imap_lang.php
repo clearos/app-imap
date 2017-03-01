1f2df0312ccfaac81e0c5c441f306849e0bf03a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:21:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP এবং POP সার্ভার সার্ভারের সাথে সংযোগ এবং / প্রদর্শন ইমেল পুনরুদ্ধার করতে মেইল ​​ক্লায়েন্ট (থান্ডারবার্ড, মেল, বিবর্তন, আউটলুক / এক্সপ্রেস ইত্যাদি) জন্য পরিষেবার একটি মান সেট প্রদান করে.';
+$lang['imap_app_name'] = 'IMAP এবং POP সার্ভার';
+$lang['imap_failed_logins'] = 'আইম্যাপ মেইল ​​লগইন ব্যর্থ';
+$lang['imap_imap'] = 'আইম্যাপ';
+$lang['imap_imaps'] = 'সিকিউর আইম্যাপ';
+$lang['imap_mail_service_invalid'] = 'মেল সেবা অবৈধ.';
+$lang['imap_pop3'] = 'পপ';
+$lang['imap_pop3s'] = 'সিকিউর পপ';
+$lang['imap_push_email'] = 'পুশ ই-মেইল';

db3863a0670a72aeebc0f5bd77872dbdc62b4b8a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:43:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP na POP server hutoa kiwango seti ya huduma kwa wateja pepe (Thunderbird, Mail, Evolution, Outlook / Express nk) kuungana na server na retrieve / kuonyesha email.';
+$lang['imap_app_name'] = 'IMAP na POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail Imeshindwa Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'salama IMAP';
+$lang['imap_mail_service_invalid'] = 'huduma ya barua ni batili.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'salama POP';
+$lang['imap_push_email'] = 'Push E-mail';

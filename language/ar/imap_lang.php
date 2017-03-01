a5c66634498ab06fc782b7369a0df750dd9adc48
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:14:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'يوفر خادم IMAP و POP مجموعة قياسية من الخدمات للعملاء البريد (طائر الرعد، بريد، تطور، توقعات / اكسبرس الخ) للاتصال الخادم واسترداد / عرض البريد الإلكتروني.';
+$lang['imap_app_name'] = 'IMAP و POP خادم';
+$lang['imap_failed_logins'] = 'فشل IMAP البريد محدودة';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP آمن';
+$lang['imap_mail_service_invalid'] = 'خدمة البريد غير صالحة.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP آمن';
+$lang['imap_push_email'] = 'البريد الإلكتروني E-دفع';

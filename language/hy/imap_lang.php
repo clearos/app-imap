3c1fcc53c6c0498918aeeaf5e1e438c449fb6f1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:16:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'The IMAP եւ POP սերվերի ապահովում է ստանդարտ շարք ծառայությունների համար փոստի հաճախորդների (Thunderbird, Mail, էվոլյուցիան, Outlook / Express եւ այլն) միացնել սերվերի եւ վերստանալ / ցուցադրման նամակ.';
+$lang['imap_app_name'] = 'IMAP եւ POP Server';
+$lang['imap_failed_logins'] = 'IMAP Փոստ ձախողված';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Փոստի ծառայություն է անվավեր:';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Հրել E-mail';

8c87b9a7480f37d7d6aa93943049457455e8f884
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:18:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP eta POP zerbitzaria eta posta-bezero (Thunderbird, Mail, Evolution, Outlook / Express abar) zerbitzu multzo estandar bat zerbitzarira konektatu eta mezu / display email eskaintzen.';
+$lang['imap_app_name'] = 'IMAP eta POP zerbitzaria';
+$lang['imap_failed_logins'] = 'IMAP Mail huts egindako sarrerak';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP segurua';
+$lang['imap_mail_service_invalid'] = 'Posta zerbitzua baliogabea da.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP Secure';
+$lang['imap_push_email'] = 'Push E-mail';

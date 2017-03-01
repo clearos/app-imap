3333c16efcf9662cdb98be9e53af0b8ede242afa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:38:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP და POP სერვერზე უზრუნველყოფს სტანდარტულ მომსახურებას ფოსტა კლიენტებს (Thunderbird, Mail, Evolution, Outlook / Express და ა.შ.), სერვერთან დაკავშირება და ახლიდან / display ელ.';
+$lang['imap_app_name'] = 'IMAP და POP სერვერი';
+$lang['imap_failed_logins'] = 'IMAP Mail ვერ Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'უსაფრთხო IMAP';
+$lang['imap_mail_service_invalid'] = 'გვერდის მომსახურება არასწორია.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'უსაფრთხო POP';
+$lang['imap_push_email'] = 'Push E-mail';

b4adaa4aca9c71f977a2710e020e298aee34c6dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:42:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Server IMAP și POP oferă un set standard de servicii pentru clienții de poștă electronică (Thunderbird, Mail, Evolution, Outlook / Express etc.) pentru a se conecta la server și de a prelua / e-mail de afișare.';
+$lang['imap_app_name'] = 'IMAP și POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail nu a reușit Conectări';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP securizat';
+$lang['imap_mail_service_invalid'] = 'Serviciul Mail este nevalid.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP securizat;';
+$lang['imap_push_email'] = 'Push e-mail';

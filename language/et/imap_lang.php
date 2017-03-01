4fa1728e684e700e4a32560d68188205efc296af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:36:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP ja POP server pakub standardsete teenuste posti kliendid (Thunderbird, Mail, Evolution, Outlook / Express jne) ühenduda serveriga ja laadida / kuva email.';
+$lang['imap_app_name'] = 'IMAP ja POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail ebaõnnestus Login&#39;id';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail teenus ei sobi.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'turvaline POP';
+$lang['imap_push_email'] = 'Push E-mail';

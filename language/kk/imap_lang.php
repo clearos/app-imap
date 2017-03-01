501eb0da31166bc5fef9671d29e6ff03f00bd0f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:38:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP және POP сервер серверге қосылу және / дисплей электрондық пошта шығарып алуға пошта клиенттер үшін қызметтердің стандартты жиынтығы (Thunderbird, Пошта, эволюция Outlook / Express және т.б.) қамтамасыз етеді.';
+$lang['imap_app_name'] = 'IMAP және POP сервер';
+$lang['imap_failed_logins'] = 'IMAP пошта Логин жаңылысы';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Қауіпсіз IMAP';
+$lang['imap_mail_service_invalid'] = 'Пошта қызметі жарамсыз болып табылады.';
+$lang['imap_pop3'] = 'ПОП';
+$lang['imap_pop3s'] = 'Қауіпсіз POP';
+$lang['imap_push_email'] = 'Push Электрондық пошта';

17a1d353af86973144ceebe91e3917659a5ef942
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:42:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP a POP server poskytuje štandardnú sadu služieb pre mailových klientov (Thunderbird, Mail, Evolution Outlook / Express podobne) na pripojenie na server a vyberať poštu / displeja.';
+$lang['imap_app_name'] = 'IMAP a POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail zlyhalo Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mailová služba je neplatný.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Push E-mail';

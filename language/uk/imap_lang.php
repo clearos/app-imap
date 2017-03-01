f58b11878cb6ffa4163d08209d2c42dd941cc060
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:44:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Сервер IMAP і POP надає стандартний набір послуг для поштових клієнтів (Thunderbird, Mail, Evolution, Outlook / Express і т.п.), щоб підключитися до сервера і отримувати електронну пошту / дисплей.';
+$lang['imap_app_name'] = 'IMAP і POP-сервер';
+$lang['imap_failed_logins'] = 'IMAP пошти невдалих спроб входу';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'безпечний IMAP';
+$lang['imap_mail_service_invalid'] = 'Поштова служба є недійсним.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'безпечний POP';
+$lang['imap_push_email'] = 'Натисніть Електронна пошта';

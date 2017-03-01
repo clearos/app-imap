ccfae86cada26695e00660935b57487c291f9335
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:38:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'ម៉ាស៊ីនបម្រើ IMAP និង POP ផ្ដល់នូវសំណុំស្តង់ដានៃការផ្តល់សេវាសម្រាប់អតិថិជនអ៊ីមែល (មយូរា, សំបុត្រ, ការវិវត្តន៍, Outlook / ប្រេសល) ដើម្បីតភ្ជាប់ទៅម៉ាស៊ីនបម្រើនិងទាញយក / បង្ហាញអ៊ីម៉ែល។';
+$lang['imap_app_name'] = 'ម៉ាស៊ីនបម្រើ IMAP និង POP';
+$lang['imap_failed_logins'] = 'ការចូលបានបរាជ័យក្នុងការ IMAP របស់សំបុត្រ';
+$lang['imap_imap'] = 'IMAP & ‧;';
+$lang['imap_imaps'] = 'IMAP & ‧; សុវត្ថិភាព';
+$lang['imap_mail_service_invalid'] = 'សេវាសំបុត្រមិនត្រឹមត្រូវ។';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP សុវត្ថិភាព';
+$lang['imap_push_email'] = 'ការជំរុញអ៊ីម៉ែល';

83fb6022f0ca489955860c805f06e1b350825670
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:43:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Nu IMAP jeung POP server nyadiakeun susunan baku ladenan pikeun klien mail (Thunderbird, surat, Evolution, Outlook / Express jsb) pikeun nyambung ka server jeung meunangkeun / pintonan email.';
+$lang['imap_app_name'] = 'IMAP jeung POP Server';
+$lang['imap_failed_logins'] = 'IMAP surat Gagal Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP aman';
+$lang['imap_mail_service_invalid'] = 'service surat téh sah.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP aman';
+$lang['imap_push_email'] = 'Push E-mail';

12f00e614007dc28bba66dcc3fc7400310ff426c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:43:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'The IMAP le Pop seva e fana ka molao-motheo sete ya ditshebeletso tsa bareki poso (Thunderbird, Mail, ho Iphetola ha Lintho, Outlook / Express joalo-joalo) ho hokela seva &#39;me lata / ne a bonahatse imeile.';
+$lang['imap_app_name'] = 'IMAP le Pop Server';
+$lang['imap_failed_logins'] = 'IMAP Mail o ile a hlōleha Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'se sireletsehileng IMAP';
+$lang['imap_mail_service_invalid'] = 'tšebeletso ea Mail ha o sebetse.';
+$lang['imap_pop3'] = 'Pop';
+$lang['imap_pop3s'] = 'se sireletsehileng Pop';
+$lang['imap_push_email'] = 'Lekhalo pakeng E-mail';

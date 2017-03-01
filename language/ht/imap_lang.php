d4c148bb9988c78e1936c1ed4300f5f0577326b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:37:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP ak POP sèvè a bay yon seri estanda nan sèvis pou kliyan lapòs (Thunderbird, Lapòs, Evolisyon, pespektiv / Express elatriye) pou konekte avèk sèvè a ak rekipere / ekspozisyon imel.';
+$lang['imap_app_name'] = 'IMAP ak POP sèvè';
+$lang['imap_failed_logins'] = 'IMAP Mail Echwe Koneksyon';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Tache imap';
+$lang['imap_mail_service_invalid'] = 'sèvis Mail se valab.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Tache POP';
+$lang['imap_push_email'] = 'Pouse E-mail';

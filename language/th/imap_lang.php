bd1282c89ed5962948bbaee6d3fd8d143204e6cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:44:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'เซิร์ฟเวอร์ IMAP และ POP ให้ชุดมาตรฐานของการบริการสำหรับลูกค้า mail (ธันเดอร์เบิร์ด, Mail, วิวัฒนาการของ Outlook / ด่วน ฯลฯ ) เพื่อเชื่อมต่อกับเซิร์ฟเวอร์และดึงอีเมล์ / จอแสดงผล';
+$lang['imap_app_name'] = 'IMAP และเซิร์ฟเวอร์ POP';
+$lang['imap_failed_logins'] = 'อีเมล IMAP ไม่สามารถเข้าสู่ระบบ';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP การรักษาความปลอดภัย';
+$lang['imap_mail_service_invalid'] = 'บริการอีเมลไม่ถูกต้อง';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP การรักษาความปลอดภัย';
+$lang['imap_push_email'] = 'อีเมล Push';

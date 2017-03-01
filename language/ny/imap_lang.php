d622a41393da86a7c135b054d0526181e3dfd262
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:28:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'The IMAP ndi Pop makina amapereka chitsanzo cha ntchito makasitomala makalata (Thunderbird, Mail, Evolution, Outlook / Express etc.) kulumikiza makina ndi kupeza / anasonyeza imelo.';
+$lang['imap_app_name'] = 'IMAP ndi Pop Server';
+$lang['imap_failed_logins'] = 'IMAP Mail Chalephera Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'otetezeka IMAP';
+$lang['imap_mail_service_invalid'] = 'utumiki Mail ndi opanda pake.';
+$lang['imap_pop3'] = 'Pop';
+$lang['imap_pop3s'] = 'otetezeka Pop';
+$lang['imap_push_email'] = 'Kankhani E-mail';

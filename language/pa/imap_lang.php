62ff3750b4e2404ca5e3db5bd40f42413a75b57e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:41:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP ਅਤੇ POP ਸਰਵਰ ਸਰਵਰ ਨਾਲ ਜੁੜਨ ਅਤੇ / ਡਿਸਪਲੇਅ ਈ-ਪ੍ਰਾਪਤ ਕਰਨ ਲਈ ਮੇਲ ਕਲਾਇਟ (ਥੰਡਰਬਰਡ, ਮੇਲ, ਈਵੇਲੂਸ਼ਨ, ਆਉਟਲੁੱਕ / ਐਕਸਪ੍ਰੈਸ ਆਦਿ) ਲਈ ਸੇਵਾ ਦਾ ਇੱਕ ਮਿਆਰੀ ਸੈੱਟ ਦਿੰਦਾ ਹੈ.';
+$lang['imap_app_name'] = 'IMAP ਅਤੇ POP ਸਰਵਰ';
+$lang['imap_failed_logins'] = 'IMAP ਦੇ ਮੇਲ ਲਾਗਇਨ ਵਿੱਚ ਅਸਫਲ';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'ਸੁਰੱਖਿਅਤ IMAP';
+$lang['imap_mail_service_invalid'] = 'ਮੇਲ ਸੇਵਾ ਗਲਤ ਹੈ.';
+$lang['imap_pop3'] = 'ਪੌਪ';
+$lang['imap_pop3s'] = 'ਸੁਰੱਖਿਅਤ POP';
+$lang['imap_push_email'] = 'ਪੁਸ਼ ਈ-ਮੇਲ';

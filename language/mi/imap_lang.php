cfe165ce422a0577ff2cf865dfc721bd6c884b3b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:40:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Te tūmau IMAP me POP whakarato te huinga paerewa o ngā ratonga mō te kiritaki mēra (Thunderbird, Mail, Evolution, Outlook / Express etc) ki hono ki te tūmau me te tiki / whakaatu īmēra.';
+$lang['imap_app_name'] = 'Tūmau IMAP me te POP';
+$lang['imap_failed_logins'] = 'I rahua IMAP Mail takiuru';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP haumaru';
+$lang['imap_mail_service_invalid'] = 'He muhu ratonga mēra.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP haumaru';
+$lang['imap_push_email'] = 'Pana E-mēra';

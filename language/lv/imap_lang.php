ede36af564a6c241d7bb0a3e48b846999270c88c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:39:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP un POP serveris nodrošina standarta pakalpojumu kopumu par pasta klientiem (Thunderbird, Mail, Evolution, Outlook / Express uc), lai izveidotu savienojumu ar serveri un paņemt / displeja e-pastu.';
+$lang['imap_app_name'] = 'IMAP un POP Server';
+$lang['imap_failed_logins'] = 'IMAP pasta Neizdevās Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Pasta pakalpojums ir nederīgs.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Push E-pasts';

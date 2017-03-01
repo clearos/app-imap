5e4a2cb41de734e5ac6db0c37e4d89fa7832e740
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:37:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Az IMAP és POP szerver biztosítja a szabványos szolgáltatások levelezőprogramok (Thunderbird, Mail, az Evolution Outlook / Express stb), hogy kapcsolódni a szerverhez, és letölthetjük / display-mailben.';
+$lang['imap_app_name'] = 'IMAP és POP szerver';
+$lang['imap_failed_logins'] = 'IMAP Mail sikertelen bejelentkezés';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail szolgáltatás érvénytelen.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'biztonságos POP';
+$lang['imap_push_email'] = 'Push E-mail';

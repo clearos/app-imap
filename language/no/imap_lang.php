872640c746aed48f5eb3e361bdb35d2579dadc2f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:41:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP og POP-serveren gir et standard sett av tjenester for e-postklienter (Thunderbird, Mail, Evolution, Outlook / Express etc.) for å koble til serveren og hente / display e-post.';
+$lang['imap_app_name'] = 'IMAP og POP-server';
+$lang['imap_failed_logins'] = 'IMAP Mail Kunne pålogginger';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Posttjenesten er ugyldig.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'sikker POP';
+$lang['imap_push_email'] = 'Push e-post';

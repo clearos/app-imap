7c809a6ce2d773e4e33eae37d0a9b0a7661897d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:37:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'The IMAP da POP uwar garken samar da wani misali sa na ayyuka na mail abokan ciniki (Thunderbird, Mail, Evolution, Outlook / Express da dai sauransu) haɗi zuwa uwar garken da kuma mai da / nuni email.';
+$lang['imap_app_name'] = 'IMAP da POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail nasarar logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail sabis ba daidai ba ne.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Tura E-mail';

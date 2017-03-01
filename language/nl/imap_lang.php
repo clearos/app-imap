8828779eb4c27e496c0c88dc87a671b258e6cdd1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:41:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'De IMAP en POP-server biedt een standaard set van diensten voor e-mail clients (Thunderbird, Mail, Evolution, Outlook / Express etc.) om verbinding met de server en op te halen / display e-mail.';
+$lang['imap_app_name'] = 'IMAP en POP-server';
+$lang['imap_failed_logins'] = 'IMAP Mail mislukte aanmeldingen';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mailservice is ongeldig.';
+$lang['imap_pop3'] = 'KNAL';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Push E-mail';

065898587afc1f8f2e95c4367b335edae7f23727
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:39:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್ ಸರ್ವರ್ ಸಂಪರ್ಕ ಮತ್ತು / ಪ್ರದರ್ಶನ ಇಮೇಲ್ ಹಿಂಪಡೆಯಲು ಮೇಲ್ ಕ್ಲೈಂಟ್ಗಳು (ತಂಡರ್, ಮೇಲ್, ವಿಕಸನ, ಔಟ್ಲುಕ್ / ಎಕ್ಸ್ಪ್ರೆಸ್ ಇತ್ಯಾದಿ) ಸೇವೆಗಳ ಒಂದು ಸಾಮಾನ್ಯ ಸೆಟ್ ಒದಗಿಸುತ್ತದೆ.';
+$lang['imap_app_name'] = 'IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್';
+$lang['imap_failed_logins'] = 'IMAP ಮೇಲ್ ವಿಫಲವಾಗಿದೆ ಲಾಗಿನ್ನುಗಳು';
+$lang['imap_imap'] = 'IMAP ಅನ್ನು';
+$lang['imap_imaps'] = 'ಸುರಕ್ಷಿತ IMAP ಅನ್ನು';
+$lang['imap_mail_service_invalid'] = 'ಮೇಲ್ ಸೇವೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['imap_pop3'] = 'ಪಾಪ್';
+$lang['imap_pop3s'] = 'ಸುರಕ್ಷಿತ ಪಾಪ್';
+$lang['imap_push_email'] = 'ಪುಷ್ ಮೇಲ್';

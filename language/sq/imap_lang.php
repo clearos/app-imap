015d9bd0519f9ba337d209ce29b9e44656f7ffe9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:13:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP dhe POP server siguron një standard i vendosur shërbimeve për klientët e postës (Thunderbird, Mail, Evolution, Outlook / Express, etj) për të lidheni me serverin dhe të marrim / ekran email.';
+$lang['imap_app_name'] = 'IMAP dhe POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail Dështoi Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP Secure';
+$lang['imap_mail_service_invalid'] = 'Shërbimi Mail është e pavlefshme.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP Secure';
+$lang['imap_push_email'] = 'Push E-mail';

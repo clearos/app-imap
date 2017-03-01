6bd1e891dcd89f74d0ccb7fa9ec929218100624e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:41:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP و POP سرور فراهم می کند یک مجموعه استاندارد از خدمات برای مشتریان پست الکترونیکی (تاندربرد، ایمیل، تکامل، چشم انداز / اکسپرس و غیره) برای اتصال به سرور و بازیابی / ایمیل ها.';
+$lang['imap_app_name'] = 'IMAP و POP سرور';
+$lang['imap_failed_logins'] = 'IMAP ایمیل نشد باری';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP امن';
+$lang['imap_mail_service_invalid'] = 'سرویس ایمیل نامعتبر است.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP امن';
+$lang['imap_push_email'] = 'فشار پست الکترونیکی';

f91af0f7f39d56d792a870c6f6e1b1f0a0b64d79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:40:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Ny IMAP sy ny POP mpizara manome ny fitsipika napetraka ny asa ho namany Sary mpanjifa (Thunderbird, Mail, Evolution, Outlook / Express sns) mba mifandray amin&#39;ny lohamilina ary retrieve / miseho mailaka.';
+$lang['imap_app_name'] = 'IMAP sy ny POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail Tsy nahomby Logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail fanompoana dia tsy manan-kery.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Manosika E-mail';

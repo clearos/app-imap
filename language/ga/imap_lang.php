4822784ae2c9218c12940c1c7644b0914310cd4f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:37:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Soláthraíonn an freastalaí IMAP agus POP sraith caighdeán na seirbhísí do chliaint phost (Thunderbird, Ríomhphost, Evolution, Outlook / Express srl) chun ceangal leis an bhfreastalaí agus a aisghabháil / r-phost taispeáint.';
+$lang['imap_app_name'] = 'IMAP agus POP Freastalaí';
+$lang['imap_failed_logins'] = 'IMAP Mail Theip logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP Secure';
+$lang['imap_mail_service_invalid'] = 'Tá seirbhís Mail neamhbhailí.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP Secure';
+$lang['imap_push_email'] = 'Brúigh Ríomhphost';

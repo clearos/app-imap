37218979eb471a18a9589a5679cff9b14ca8c499
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:37:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP અને પીઓપી સર્વર સાથે જોડાવા માટે અને / પ્રદર્શન ઇમેઇલ પ્રાપ્ત કરવા માટે મેલ ક્લાઈન્ટો (થંડરબર્ડ, મેઇલ, વિકસિત થવું, આઉટલુક / એક્સપ્રેસ વગેરે) માટે સેવાઓ પ્રમાણભૂત સમૂહ પૂરો પાડે છે.';
+$lang['imap_app_name'] = 'IMAP અને પીઓપી સર્વર';
+$lang['imap_failed_logins'] = 'IMAP મેઈલ પ્રવેશ અા નિષ્ફળ';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'સુરક્ષિત IMAP';
+$lang['imap_mail_service_invalid'] = 'મેઇલ સેવા અમાન્ય છે.';
+$lang['imap_pop3'] = 'પીઓપી';
+$lang['imap_pop3s'] = 'સુરક્ષિત પીઓપી';
+$lang['imap_push_email'] = 'પુશ ઇ-મેલ';

8b0c654c4ce310313f535f615b6b6afa46fd3f09
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:41:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Il IMAP u POP server jipprovdi sett standard ta &#39;servizzi għall-klijenti tal-posta (Thunderbird, Mail, Evoluzzjoni, Outlook / Express eċċ) li jgħaqqdu l-server u jerġgħu jieħdu lura / email wiri.';
+$lang['imap_app_name'] = 'IMAP u POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail rnexxielhomx logins';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP sikura';
+$lang['imap_mail_service_invalid'] = 'servizz tal-posta huwa invalidu.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP sikura';
+$lang['imap_push_email'] = 'Imbotta E-mail';

71ddd047a216898604c851e06616c002c6aafacd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:44:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'Дар IMAP ва POP сервер таъмин маҷмӯи стандартии хизматрасонӣ барои муштариён почтаи (Thunderbird, Почта, Evolution, Outlook / Express ва ғайра) барои пайваст шудан ба сервер ва ҷустуҷӯи / почтаи кунед.';
+$lang['imap_app_name'] = 'IMAP ва POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail муяссар рамзњои';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'IMAP Амн';
+$lang['imap_mail_service_invalid'] = 'Хадамоти Mail нодуруст аст.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP Амн';
+$lang['imap_push_email'] = 'Push электронии';

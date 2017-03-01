da36c3e084ec281c08b2ef2458c042557584ec10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 15:43:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/imap_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['imap_app_description'] = 'IMAP மற்றும் POP சர்வர் சர்வர் இணைக்க மற்றும் / காட்சி மின்னஞ்சல் மீட்டெடுக்க மின்னஞ்சல் வாடிக்கையாளர்கள் (தண்டர்பேர்ட், மெயில், பரிணாமம், அவுட்லுக் / எக்ஸ்பிரஸ் முதலியன) சேவைகள் ஒரு நிலையான அமைப்பு வழங்குகிறது.';
+$lang['imap_app_name'] = 'IMAP மற்றும் POP சர்வர்';
+$lang['imap_failed_logins'] = ', IMAP மின்னஞ்சல் உள்நுழைவுகள் தோல்வி';
+$lang['imap_imap'] = 'ஐஎம்ஏபி';
+$lang['imap_imaps'] = 'பாதுகாப்பான, IMAP';
+$lang['imap_mail_service_invalid'] = 'மின்னஞ்சல் சேவை தவறானது.';
+$lang['imap_pop3'] = 'பாப்';
+$lang['imap_pop3s'] = 'பாதுகாப்பான, POP';
+$lang['imap_push_email'] = 'Push e-mail';

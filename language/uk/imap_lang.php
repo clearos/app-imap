98d922968922351aa31b802e6e6d618870dfdee4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:33:15 +0000

    Add new files and directories, commit changes, push changes

--- language/uk/imap_lang.php
+++ language/uk/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Сервер IMAP і POP надає стандартний набір послуг для поштових клієнтів (Thunderbird, Mail, Evolution, Outlook / Express і т.п.), щоб підключитися до сервера і отримувати електронну пошту / дисплей.';
-$lang['imap_app_name'] = 'IMAP і POP-сервер';
-$lang['imap_failed_logins'] = 'IMAP пошти невдалих спроб входу';
+$lang['imap_app_description'] = 'Сервер IMAP та POP забезпечує стандартний набір сервісів для поштових клієнтів (Thunderbird, Mail, Evolution, Outlook / Express тощо) для підключення до сервера та отримання / відображення електронної пошти.';
+$lang['imap_app_name'] = 'IMAP та POP-сервер';
+$lang['imap_failed_logins'] = 'IMAP поштові помилки входу';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'безпечний IMAP';
-$lang['imap_mail_service_invalid'] = 'Поштова служба є недійсним.';
+$lang['imap_imaps'] = 'Захистіть IMAP';
+$lang['imap_mail_service_invalid'] = 'Поштовий сервіс недійсний.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'безпечний POP';
-$lang['imap_push_email'] = 'Натисніть Електронна пошта';
+$lang['imap_pop3s'] = 'Безпечний POP';
+$lang['imap_push_email'] = 'Натиснути на електронну пошту';

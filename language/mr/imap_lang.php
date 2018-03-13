83d2b50efd632d6a4dc48cc5429e34140804c7ab
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:16:28 +0000

    Add new files and directories, commit changes, push changes

--- language/mr/imap_lang.php
+++ language/mr/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP आणि POP सर्व्हर सर्व्हर कनेक्ट आणि / प्रदर्शन ई-मेल पुनर्प्राप्त करण्यासाठी मेल क्लायंट (थंडरबर्ड, मेल, उत्क्रांती, आउटलुक / एक्सप्रेस इ) सेवा मानक उपलब्ध आहे.';
+$lang['imap_app_description'] = 'सर्व्हरशी कनेक्ट करण्यासाठी आणि ईमेल / पुनर्प्राप्ती ईमेलसाठी IMAP आणि POP सर्व्हर मेल क्लायंट्स (थंडरबर्ड, मेल, इव्होल्यूशन, आउटलुक / एक्सप्रेस इ.) साठी मानक सेवा प्रदान करते.';
 $lang['imap_app_name'] = 'IMAP आणि POP सर्व्हर';
-$lang['imap_failed_logins'] = 'IMAP मेल लॉगिन अपयशी';
+$lang['imap_failed_logins'] = 'IMAP मेल अयशस्वी लॉगिन';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'सुरक्षित IMAP';
-$lang['imap_mail_service_invalid'] = 'मेल सेवा अवैध आहे.';
+$lang['imap_mail_service_invalid'] = 'मेल सेवा अवैध आहे';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'सुरक्षित POP';
-$lang['imap_push_email'] = 'पुश ई-मेल';
+$lang['imap_pop3s'] = 'सुरक्षित पीओपी';
+$lang['imap_push_email'] = 'ई-मेल पुश करा';

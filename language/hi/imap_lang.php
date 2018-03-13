f66e62acccf5cba5caca5c0226c2d7fcf38e7c8f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:02:37 +0000

    Add new files and directories, commit changes, push changes

--- language/hi/imap_lang.php
+++ language/hi/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'आईमैप और पॉप सर्वर सर्वर से कनेक्ट करने और / प्रदर्शन ईमेल पुनः प्राप्त करने के लिए मेल क्लाइंट (थंडरबर्ड, मेल, विकास, आउटलुक / एक्सप्रेस आदि) के लिए सेवाओं की एक मानक सेट प्रदान करता है।';
-$lang['imap_app_name'] = 'आईमैप और पॉप सर्वर';
-$lang['imap_failed_logins'] = 'आईमैप मेल के लॉगिन विफल';
-$lang['imap_imap'] = 'आईमैप';
-$lang['imap_imaps'] = 'सुरक्षित आईमैप';
-$lang['imap_mail_service_invalid'] = 'मेल सेवा अमान्य है।';
+$lang['imap_app_description'] = 'आईएमएपी और पीओपी सर्वर मेल क्लाइंट (थंडरबर्ड, मेल, इवोल्यूशन, आउटलुक / एक्सप्रेस आदि) के लिए सर्वर से कनेक्ट करने और पुनः प्राप्त / प्रदर्शन ईमेल के लिए एक मानक सेवा प्रदान करता है।';
+$lang['imap_app_name'] = 'IMAP और POP सर्वर';
+$lang['imap_failed_logins'] = 'IMAP मेल विफल लॉगिन';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'सुरक्षित IMAP';
+$lang['imap_mail_service_invalid'] = 'मेल सेवा अमान्य है';
 $lang['imap_pop3'] = 'पॉप';
 $lang['imap_pop3s'] = 'सुरक्षित पॉप';
-$lang['imap_push_email'] = 'पुश ई-मेल';
+$lang['imap_push_email'] = 'ई-मेल पुश करें';

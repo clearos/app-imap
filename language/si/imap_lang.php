b516cf3b9d485bc2b5c1c50d4239eb7c49fa87aa
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:24:28 +0000

    Add new files and directories, commit changes, push changes

--- language/si/imap_lang.php
+++ language/si/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'මෙම IMAP හා POP සේවාදායකය සේවාදායකය වෙත සම්බන්ධ වීමට සහ / සංදර්ශකය ඊ-තැපැල් ලබා ගැනීමට තැපැල් ගනුදෙනුකරුවන් සඳහා සේවා සම්මත කට්ටලයක් (තණ්ඩර්බර්ඩ්, මේල්, පරිණාමය, ඉදිරි දැක්මක් / එක්ස්ප්රස් ආදිය) සපයයි.';
-$lang['imap_app_name'] = 'IMAP හා POP සේවාදායකය';
-$lang['imap_failed_logins'] = 'IMAP තැපැල් හමුවිය අසාර්ථක විය';
+$lang['imap_app_description'] = 'IMAP සහ POP සේවාදායකය සේවාදායකයට සම්බන්ධ වීම හා ඊමේල් ලබාගැනීම / විදහා දැක්වීම සඳහා තැපැල් සේවාලාභීන් සඳහා තන්තු සේවා (Thunderbird, Mail, Evolution, Outlook / Express ආදිය ආදිය) ලබා දෙයි.';
+$lang['imap_app_name'] = 'IMAP සහ POP සේවාදායකය';
+$lang['imap_failed_logins'] = 'IMAP තැපැල් ලොගින් අසාර්ථක විය';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'ආරක්ෂිත IMAP';
-$lang['imap_mail_service_invalid'] = 'තැපැල් සේවා වලංගු නැත.';
+$lang['imap_mail_service_invalid'] = 'තැපැල් සේවාව වලංගු නැත.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'ආරක්ෂිත POP';
-$lang['imap_push_email'] = 'තල්ලුව ඊ-තැපැල';
+$lang['imap_push_email'] = 'ඊ-තැපෑලෙන් එවන්න';

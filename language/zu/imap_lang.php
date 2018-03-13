e3dce8d80be21e3e08286665fb5c81847da99a9b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:37:29 +0000

    Add new files and directories, commit changes, push changes

--- language/zu/imap_lang.php
+++ language/zu/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'I-IMAP futhi POP iseva inikeza iqoqo nezinga lemisebenzi imeyili amaklayenti (Thunderbird, Mail, Evolution, Outlook / Express njll) ukuxhuma kwiseva futhi ukubuyisa / ukuveza imeyili.';
-$lang['imap_app_name'] = 'IMAP futhi POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Yehlulekile ungene';
-$lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP Secure';
-$lang['imap_mail_service_invalid'] = 'Isevisi email address is invalid.';
-$lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP Secure';
+$lang['imap_app_description'] = 'Iseva ye-IMAP ne-POP inikeza isevisi ejwayelekile yamasevisi amaklayenti (Thunderbird, Mail, Evolution, Outlook / Express njll) ukuxhuma kuseva bese uthola / ukubonisa i-imeyili.';
+$lang['imap_app_name'] = 'Iseva ye-IMAP ne-POP';
+$lang['imap_failed_logins'] = 'I-IMAP Mail Yehlulekile ukungena ngemvume';
+$lang['imap_imap'] = 'I-IMAP';
+$lang['imap_imaps'] = 'I-IMAP ephephile';
+$lang['imap_mail_service_invalid'] = 'Isevisi yemeyili ayivumelekile.';
+$lang['imap_pop3'] = 'I-POP';
+$lang['imap_pop3s'] = 'I-POP ephephile';
 $lang['imap_push_email'] = 'Push E-mail';

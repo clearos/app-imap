5541b4da9cd0283f169baeb92e739191c2017cfd
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:12:07 +0000

    Add new files and directories, commit changes, push changes

--- language/lt/imap_lang.php
+++ language/lt/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP ir POP serveris suteikia standartinį rinkinį paslaugų pašto klientai (Thunderbird, Mail, evoliucija, Outlook / Express ir tt), prisijungti prie serverio ir gauti / Vaizdo laišką.';
-$lang['imap_app_name'] = 'IMAP ir POP serverio';
-$lang['imap_failed_logins'] = 'Pašto IMAP nesėkmingų prisijungimų';
+$lang['imap_app_description'] = 'IMAP ir POP serveris suteikia standartinį pašto klientų paslaugų komplektą ("Thunderbird", "Mail", "Evolution", "Outlook / Express" ir tt), norint prisijungti prie serverio ir gauti ar rodyti el. Laiškus.';
+$lang['imap_app_name'] = 'IMAP ir POP serveris';
+$lang['imap_failed_logins'] = 'IMAP pašto nepavyko prisijungimai';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'Saugus IMAP';
-$lang['imap_mail_service_invalid'] = 'Pašto paslauga yra neteisinga.';
-$lang['imap_pop3'] = 'Pop';
-$lang['imap_pop3s'] = 'Saugus Pop';
-$lang['imap_push_email'] = '"Push E-mail';
+$lang['imap_mail_service_invalid'] = 'Pašto paslaugos neteisingos.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'Saugus POP';
+$lang['imap_push_email'] = 'Push E-mail';

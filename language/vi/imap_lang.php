1142998627c172a789bc84b6c04aebd0d670289e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:35:00 +0000

    Add new files and directories, commit changes, push changes

--- language/vi/imap_lang.php
+++ language/vi/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Các IMAP và POP server cung cấp một bộ tiêu chuẩn của dịch vụ cho các khách hàng mail (Thunderbird, Mail, Evolution, Outlook / tốc vv) để kết nối đến máy chủ và lấy / hiển thị email.';
-$lang['imap_app_name'] = 'IMAP và POP server';
-$lang['imap_failed_logins'] = 'IMAP Thư Không Logins';
+$lang['imap_app_description'] = 'Máy chủ IMAP và POP cung cấp một bộ dịch vụ tiêu chuẩn cho các ứng dụng thư (Thunderbird, Mail, Evolution, Outlook / Express vv) để kết nối với máy chủ và lấy / hiển thị email.';
+$lang['imap_app_name'] = 'Máy chủ IMAP và POP';
+$lang['imap_failed_logins'] = 'Thư nhập IMAP thất bại';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP an toàn';
-$lang['imap_mail_service_invalid'] = 'dịch vụ email không hợp lệ.';
+$lang['imap_imaps'] = 'Bảo mật IMAP';
+$lang['imap_mail_service_invalid'] = 'Dịch vụ thư không hợp lệ.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'POP an toàn';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_push_email'] = 'Đẩy E-mail';

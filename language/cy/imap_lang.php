1e32976671191697d98d4154990248ee1d20265e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:35:49 +0000

    Add new files and directories, commit changes, push changes

--- language/cy/imap_lang.php
+++ language/cy/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Mae&#39;r gweinydd IMAP a POP yn darparu set safonol o wasanaethau i gleientiaid bost (Thunderbird, Mail, Evolution, Outlook / Express ac ati) er mwyn cysylltu â&#39;r gweinydd ac adfer / e-bost arddangos.';
-$lang['imap_app_name'] = 'IMAP a POP Gweinydd';
-$lang['imap_failed_logins'] = 'Methwyd IMAP Mail mewngofnodi';
+$lang['imap_app_description'] = 'Mae&#39;r gweinydd IMAP a POP yn darparu set safonol o wasanaethau ar gyfer cleientiaid post (Thunderbird, Mail, Evolution, Outlook / Express ac ati) i gysylltu â&#39;r gweinydd ac adfer / arddangos e-bost.';
+$lang['imap_app_name'] = 'IMAP a Gweinyddwr POP';
+$lang['imap_failed_logins'] = 'Mewngofnodi IMAP Mail Wedi&#39;i Fethu';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP diogel';
-$lang['imap_mail_service_invalid'] = 'gwasanaeth post yn annilys.';
+$lang['imap_imaps'] = 'IMAP Diogel';
+$lang['imap_mail_service_invalid'] = 'Mae&#39;r gwasanaeth post yn annilys.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP diogel';
+$lang['imap_pop3s'] = 'POP Diogel';
 $lang['imap_push_email'] = 'Gwthiwch E-bost';

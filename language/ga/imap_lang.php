33a96830edb52d5703caea606c3e5020a3b80f0c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:04:34 +0000

    Add new files and directories, commit changes, push changes

--- language/ga/imap_lang.php
+++ language/ga/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Soláthraíonn an freastalaí IMAP agus POP sraith caighdeán na seirbhísí do chliaint phost (Thunderbird, Ríomhphost, Evolution, Outlook / Express srl) chun ceangal leis an bhfreastalaí agus a aisghabháil / r-phost taispeáint.';
-$lang['imap_app_name'] = 'IMAP agus POP Freastalaí';
-$lang['imap_failed_logins'] = 'IMAP Mail Theip logins';
+$lang['imap_app_description'] = 'Soláthraíonn an freastalaí IMAP agus POP sraith sheirbhísí caighdeánach do chliaint ríomhphoist (Thunderbird, Mail, Evolution, Outlook / Express, etc.) chun ceangal leis an bhfreastalaí agus ríomhphost a fháil / a thaispeáint.';
+$lang['imap_app_name'] = 'IMAP agus Freastalaí POP';
+$lang['imap_failed_logins'] = 'Logáil isteach IMAP Mail Failed';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'IMAP Secure';
-$lang['imap_mail_service_invalid'] = 'Tá seirbhís Mail neamhbhailí.';
+$lang['imap_mail_service_invalid'] = 'Tá an tseirbhís phoist neamhbhailí.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP Secure';
-$lang['imap_push_email'] = 'Brúigh Ríomhphost';
+$lang['imap_pop3s'] = 'POP slán';
+$lang['imap_push_email'] = 'Brúigh r-phost';

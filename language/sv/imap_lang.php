40cadd29053ec004f069710a64bd4e0d87873320
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:29:06 +0000

    Add new files and directories, commit changes, push changes

--- language/sv/imap_lang.php
+++ language/sv/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP och POP-servern tillhandahåller en standarduppsättning av tjänster för e-postklienter (Thunderbird, Mail, Evolution, Outlook / Express etc.) för att ansluta till servern och hämta / display e-post.';
-$lang['imap_app_name'] = 'IMAP och POP-server';
-$lang['imap_failed_logins'] = 'IMAP Mail misslyckades inloggningar';
+$lang['imap_app_description'] = 'IMAP- och POP-servern tillhandahåller en standard uppsättning tjänster för postklienter (Thunderbird, Mail, Evolution, Outlook / Express etc.) för att ansluta till servern och hämta / visa e-post.';
+$lang['imap_app_name'] = 'IMAP och POP-servern';
+$lang['imap_failed_logins'] = 'IMAP Mail misslyckades påloggningar';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'säker IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail-tjänsten är ogiltigt.';
+$lang['imap_imaps'] = 'Säkra IMAP';
+$lang['imap_mail_service_invalid'] = 'Posttjänsten är ogiltig.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'säker POP';
-$lang['imap_push_email'] = 'Push E-post';
+$lang['imap_pop3s'] = 'Säker POP';
+$lang['imap_push_email'] = 'Tryck E-post';

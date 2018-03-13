01fe5f6a719757150be3a1750467f8aec11267eb
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:03:16 +0000

    Add new files and directories, commit changes, push changes

--- language/hu/imap_lang.php
+++ language/hu/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Az IMAP és POP szerver biztosítja a szabványos szolgáltatások levelezőprogramok (Thunderbird, Mail, az Evolution Outlook / Express stb), hogy kapcsolódni a szerverhez, és letölthetjük / display-mailben.';
+$lang['imap_app_description'] = 'Az IMAP és a POP szerver szabványos szolgáltatáskészletet nyújt a levelezőprogramok (Thunderbird, Mail, Evolution, Outlook / Express stb.) Számára a kiszolgálóhoz való kapcsolódáshoz és e-mailek letöltéséhez / megjelenítéséhez.';
 $lang['imap_app_name'] = 'IMAP és POP szerver';
-$lang['imap_failed_logins'] = 'IMAP Mail sikertelen bejelentkezés';
+$lang['imap_failed_logins'] = 'IMAP-mail sikertelen bejelentkezés';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail szolgáltatás érvénytelen.';
+$lang['imap_imaps'] = 'Biztonságos IMAP';
+$lang['imap_mail_service_invalid'] = 'A postai szolgáltatás érvénytelen.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'biztonságos POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_pop3s'] = 'Biztonságos POP';
+$lang['imap_push_email'] = 'Nyomja meg az e-mailt';

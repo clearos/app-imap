4338489eb7b33329e7806e50aa33118336a77094
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:13:59 +0000

    Add new files and directories, commit changes, push changes

--- language/mi/imap_lang.php
+++ language/mi/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Te tūmau IMAP me POP whakarato te huinga paerewa o ngā ratonga mō te kiritaki mēra (Thunderbird, Mail, Evolution, Outlook / Express etc) ki hono ki te tūmau me te tiki / whakaatu īmēra.';
-$lang['imap_app_name'] = 'Tūmau IMAP me te POP';
-$lang['imap_failed_logins'] = 'I rahua IMAP Mail takiuru';
+$lang['imap_app_description'] = 'E whakaratohia ana e te tūmau IMAP me te POP he raupapa paerewa mo nga ratonga mēra (Thunderbird, Mail, Evolution, Outlook / Express me ētahi atu) hei hono atu ki te tūmau me te tiki / īmēra whakaatu.';
+$lang['imap_app_name'] = 'IMAP me te POP Tūmau';
+$lang['imap_failed_logins'] = 'I rahua te Hainatanga Mail IMAP';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP haumaru';
-$lang['imap_mail_service_invalid'] = 'He muhu ratonga mēra.';
+$lang['imap_imaps'] = 'IMAP Haumaru';
+$lang['imap_mail_service_invalid'] = 'He muhu te ratonga Mēra.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP haumaru';
+$lang['imap_pop3s'] = 'POP Haumaru';
 $lang['imap_push_email'] = 'Pana E-mēra';

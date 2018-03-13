140af4777e64d05d7ae4f2417c38d48900d279c7
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:46:03 +0000

    Add new files and directories, commit changes, push changes

--- language/sq/imap_lang.php
+++ language/sq/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP dhe POP server siguron një standard i vendosur shërbimeve për klientët e postës (Thunderbird, Mail, Evolution, Outlook / Express, etj) për të lidheni me serverin dhe të marrim / ekran email.';
+$lang['imap_app_description'] = 'IMAP dhe serveri POP ofrojnë një grup standard shërbimesh për klientët e postës (Thunderbird, Mail, Evolution, Outlook / Express etj.) Për t&#39;u lidhur me serverin dhe për të rimarrë / shfaqur emailin.';
 $lang['imap_app_name'] = 'IMAP dhe POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Dështoi Logins';
+$lang['imap_failed_logins'] = 'IMAP Mail Failed Logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP Secure';
-$lang['imap_mail_service_invalid'] = 'Shërbimi Mail është e pavlefshme.';
+$lang['imap_imaps'] = 'Siguro IMAP';
+$lang['imap_mail_service_invalid'] = 'Shërbimi i postës është i pavlefshëm.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP Secure';
+$lang['imap_pop3s'] = 'Sigurt POP';
 $lang['imap_push_email'] = 'Push E-mail';

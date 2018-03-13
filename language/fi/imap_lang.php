248b6989e3926e28f620c656a073f656329a1f7d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:57:25 +0000

    Add new files and directories, commit changes, push changes

--- language/fi/imap_lang.php
+++ language/fi/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP ja POP-palvelin standardi asettaa palvelujen sähköpostiohjelmien (Thunderbird, Mail, Evolution, Outlook / Express jne) muodostaa yhteyden palvelimeen ja noutaa / näyttö email.';
-$lang['imap_app_name'] = 'IMAP ja POP-palvelin';
-$lang['imap_failed_logins'] = 'IMAP Mail epäonnistui Logins';
+$lang['imap_app_description'] = 'IMAP- ja POP-palvelin tarjoaa standardipalveluja sähköpostiohjelmille (Thunderbird, Mail, Evolution, Outlook / Express jne.) Yhteyden muodostamiseen palvelimeen ja sähköpostin noutamiseen / näyttämiseen.';
+$lang['imap_app_name'] = 'IMAP- ja POP-palvelin';
+$lang['imap_failed_logins'] = 'IMAP-sähköpostin epäonnistuneet kirjautumiset';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail -palvelu on virheellinen.';
+$lang['imap_imaps'] = 'Suojattu IMAP';
+$lang['imap_mail_service_invalid'] = 'Mail-palvelu on virheellinen.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'Secure POP';
 $lang['imap_push_email'] = 'Push E-mail';

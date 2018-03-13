0fd2e75a3f47c0583eeba1b4f1a4ad250d8484ba
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:51:08 +0000

    Add new files and directories, commit changes, push changes

--- language/bg/imap_lang.php
+++ language/bg/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Сървърът за IMAP и POP предоставя стандартен набор от услуги за пощенски клиенти (Thunderbird, Mail, Evolution, Outlook / Express и т.н.), за да се свържете със сървъра и извличане / дисплей имейл.';
+$lang['imap_app_description'] = 'Сървърът IMAP и POP предоставя стандартен набор от услуги за пощенски клиенти (Thunderbird, Mail, Evolution, Outlook / Express и др.), За да се свържете със сървъра и да изтеглите / покажете имейл.';
 $lang['imap_app_name'] = 'IMAP и POP сървър';
-$lang['imap_failed_logins'] = 'IMAP Mail Неуспешно Logins';
+$lang['imap_failed_logins'] = 'Неуспешно влизане в IMAP поща';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail услуга е невалиден.';
+$lang['imap_imaps'] = 'Защитете IMAP';
+$lang['imap_mail_service_invalid'] = 'Пощата е невалидна.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Secure POP';
-$lang['imap_push_email'] = 'Push E-мейл';
+$lang['imap_pop3s'] = 'Защитено POP';
+$lang['imap_push_email'] = 'Натиснете E-mail';

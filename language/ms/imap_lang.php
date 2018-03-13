24caae66d25a942fdb3f6968b5f884014dc297ec
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:17:07 +0000

    Add new files and directories, commit changes, push changes

--- language/ms/imap_lang.php
+++ language/ms/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'The IMAP dan POP server menyediakan satu set standard perkhidmatan untuk pelanggan mel (Thunderbird, Mail, Evolution, Outlook / Express dan lain-lain) untuk menyambung kepada pelayan dan mengambil e-mel / paparan.';
-$lang['imap_app_name'] = 'IMAP dan POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Gagal Logins';
+$lang['imap_app_description'] = 'Pelayan IMAP dan POP menyediakan set perkhidmatan standard untuk pelanggan mel (Thunderbird, Mail, Evolution, Outlook / Express dll) untuk menyambung ke pelayan dan mengambil / memaparkan e-mel.';
+$lang['imap_app_name'] = 'Pelayan IMAP dan POP';
+$lang['imap_failed_logins'] = 'Log masuk Gagal IMAP';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP selamat';
-$lang['imap_mail_service_invalid'] = 'perkhidmatan mel tidak sah.';
+$lang['imap_imaps'] = 'Selamat IMAP';
+$lang['imap_mail_service_invalid'] = 'Perkhidmatan mel tidak sah.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP selamat';
-$lang['imap_push_email'] = 'Push E-mel';
+$lang['imap_pop3s'] = 'Selamat POP';
+$lang['imap_push_email'] = 'Tolak E-mel';

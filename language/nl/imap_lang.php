a9cae5eb8ee06ca34f53d3b69016d3d3af659f08
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:18:57 +0000

    Add new files and directories, commit changes, push changes

--- language/nl/imap_lang.php
+++ language/nl/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'De IMAP en POP-server biedt een standaard set van diensten voor e-mail clients (Thunderbird, Mail, Evolution, Outlook / Express etc.) om verbinding met de server en op te halen / display e-mail.';
-$lang['imap_app_name'] = 'IMAP en POP-server';
-$lang['imap_failed_logins'] = 'IMAP Mail mislukte aanmeldingen';
+$lang['imap_app_description'] = 'De IMAP- en POP-server biedt een standaardset van services voor e-mailclients (Thunderbird, Mail, Evolution, Outlook / Express etc.) om verbinding te maken met de server en e-mail op te halen / weer te geven.';
+$lang['imap_app_name'] = 'IMAP- en POP-server';
+$lang['imap_failed_logins'] = 'IMAP e-mail mislukte aanmeldingen';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mailservice is ongeldig.';
+$lang['imap_imaps'] = 'Beveilig IMAP';
+$lang['imap_mail_service_invalid'] = 'E-mailservice is ongeldig.';
 $lang['imap_pop3'] = 'KNAL';
-$lang['imap_pop3s'] = 'Secure POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_pop3s'] = 'Beveiligde POP';
+$lang['imap_push_email'] = 'Druk op E-mail';

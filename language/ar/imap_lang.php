e9a1e34a4d10870bc0795c6a407e612396600a32
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:46:29 +0000

    Add new files and directories, commit changes, push changes

--- language/ar/imap_lang.php
+++ language/ar/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'يوفر خادم IMAP و POP مجموعة قياسية من الخدمات للعملاء البريد (طائر الرعد، بريد، تطور، توقعات / اكسبرس الخ) للاتصال الخادم واسترداد / عرض البريد الإلكتروني.';
-$lang['imap_app_name'] = 'IMAP و POP خادم';
-$lang['imap_failed_logins'] = 'فشل IMAP البريد محدودة';
+$lang['imap_app_description'] = 'يوفر خادم IMAP و POP مجموعة قياسية من الخدمات لعملاء البريد (Thunderbird و Mail و Evolution و Outlook / Express وما إلى ذلك) للاتصال بالخادم واسترداد / عرض البريد الإلكتروني.';
+$lang['imap_app_name'] = 'خادم IMAP و POP';
+$lang['imap_failed_logins'] = 'بريد IMAP فشل في تسجيل الدخول';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP آمن';
+$lang['imap_imaps'] = 'تأمين الوصول عبر IMAP';
 $lang['imap_mail_service_invalid'] = 'خدمة البريد غير صالحة.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP آمن';
-$lang['imap_push_email'] = 'البريد الإلكتروني E-دفع';
+$lang['imap_pop3s'] = 'آمن POP';
+$lang['imap_push_email'] = 'دفع البريد الإلكتروني';

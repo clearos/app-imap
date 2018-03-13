3d879e7bc52e73dc9c1203af7c04cd3dce4cf67b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:00:14 +0000

    Add new files and directories, commit changes, push changes

--- language/gu/imap_lang.php
+++ language/gu/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP અને પીઓપી સર્વર સાથે જોડાવા માટે અને / પ્રદર્શન ઇમેઇલ પ્રાપ્ત કરવા માટે મેલ ક્લાઈન્ટો (થંડરબર્ડ, મેઇલ, વિકસિત થવું, આઉટલુક / એક્સપ્રેસ વગેરે) માટે સેવાઓ પ્રમાણભૂત સમૂહ પૂરો પાડે છે.';
-$lang['imap_app_name'] = 'IMAP અને પીઓપી સર્વર';
-$lang['imap_failed_logins'] = 'IMAP મેઈલ પ્રવેશ અા નિષ્ફળ';
+$lang['imap_app_description'] = 'IMAP અને POP સર્વર મેલ ગ્રાહકો (થંડરબર્ડ, મેલ, ઇવોલ્યુશન, આઉટલુક / એક્સપ્રેસ વગેરે) માટે સર્વર સાથે કનેક્ટ કરવા અને ઇમેઇલ / પુનઃપ્રાપ્ત ઇમેઇલ માટે સેવાઓનો એક માનક સેટ પ્રદાન કરે છે.';
+$lang['imap_app_name'] = 'IMAP અને POP સર્વર';
+$lang['imap_failed_logins'] = 'IMAP મેઇલ નિષ્ફળ લૉગિન';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'સુરક્ષિત IMAP';
-$lang['imap_mail_service_invalid'] = 'મેઇલ સેવા અમાન્ય છે.';
+$lang['imap_mail_service_invalid'] = 'મેઇલ સેવા અમાન્ય છે';
 $lang['imap_pop3'] = 'પીઓપી';
 $lang['imap_pop3s'] = 'સુરક્ષિત પીઓપી';
-$lang['imap_push_email'] = 'પુશ ઇ-મેલ';
+$lang['imap_push_email'] = 'ઈ-મેલ દબાણ કરો';

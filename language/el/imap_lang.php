cdde2a159165093bfc3a0eb1d44f55d1ccf1b312
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:59:49 +0000

    Add new files and directories, commit changes, push changes

--- language/el/imap_lang.php
+++ language/el/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Ο διακομιστής IMAP και POP παρέχει ένα τυποποιημένο σύνολο των υπηρεσιών για τους πελάτες ηλεκτρονικού ταχυδρομείου (Thunderbird, Mail, Evolution, Outlook / Express, κλπ) για να συνδεθείτε στο διακομιστή και να ανακτήσετε / email οθόνη.';
-$lang['imap_app_name'] = 'IMAP και POP διακομιστή';
-$lang['imap_failed_logins'] = 'IMAP Mail Αποτυχία Συνδέσεις';
+$lang['imap_app_description'] = 'Ο διακομιστής IMAP και POP παρέχει ένα τυπικό σύνολο υπηρεσιών για προγράμματα-πελάτες αλληλογραφίας (Thunderbird, Mail, Evolution, Outlook / Express κ.λπ.) για σύνδεση με το διακομιστή και ανάκτηση / εμφάνιση μηνυμάτων ηλεκτρονικού ταχυδρομείου.';
+$lang['imap_app_name'] = 'Διακομιστή IMAP και POP';
+$lang['imap_failed_logins'] = 'Η αποτυχημένη σύνδεση των μηνυμάτων IMAP';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'ασφαλής IMAP';
-$lang['imap_mail_service_invalid'] = 'υπηρεσία ηλεκτρονικού ταχυδρομείου είναι έγκυρη.';
+$lang['imap_imaps'] = 'Ασφαλής IMAP';
+$lang['imap_mail_service_invalid'] = 'Η υπηρεσία αλληλογραφίας δεν είναι έγκυρη.';
 $lang['imap_pop3'] = 'ΚΡΟΤΟΣ';
-$lang['imap_pop3s'] = 'ασφαλής POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_pop3s'] = 'Ασφαλής POP';
+$lang['imap_push_email'] = 'Πιέστε το E-mail';

28e50f6d8bcfd8efcf572f8a8a184ef04522808f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:01:07 +0000

    Add new files and directories, commit changes, push changes

--- language/ht/imap_lang.php
+++ language/ht/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP ak POP sèvè a bay yon seri estanda nan sèvis pou kliyan lapòs (Thunderbird, Lapòs, Evolisyon, pespektiv / Express elatriye) pou konekte avèk sèvè a ak rekipere / ekspozisyon imel.';
+$lang['imap_app_description'] = 'IMAP ak POP sèvè a bay yon seri estanda sèvis pou kliyan lapòs (Thunderbird, Mail, Evolisyon, Outlook / Express elatriye) pou konekte avèk sèvè a ak rekipere / imèl ekspozisyon.';
 $lang['imap_app_name'] = 'IMAP ak POP sèvè';
-$lang['imap_failed_logins'] = 'IMAP Mail Echwe Koneksyon';
+$lang['imap_failed_logins'] = 'IMAP Mail Echwe Logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Tache imap';
-$lang['imap_mail_service_invalid'] = 'sèvis Mail se valab.';
+$lang['imap_imaps'] = 'Tache IMAP';
+$lang['imap_mail_service_invalid'] = 'Lapòs Lapòs se envalid.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'Tache POP';
-$lang['imap_push_email'] = 'Pouse E-mail';
+$lang['imap_push_email'] = 'Pouse e-mail';

40ebcc12e9173bb067e33e6df5285b351e295aed
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:29:50 +0000

    Add new files and directories, commit changes, push changes

--- language/ta/imap_lang.php
+++ language/ta/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP மற்றும் POP சர்வர் சர்வர் இணைக்க மற்றும் / காட்சி மின்னஞ்சல் மீட்டெடுக்க மின்னஞ்சல் வாடிக்கையாளர்கள் (தண்டர்பேர்ட், மெயில், பரிணாமம், அவுட்லுக் / எக்ஸ்பிரஸ் முதலியன) சேவைகள் ஒரு நிலையான அமைப்பு வழங்குகிறது.';
-$lang['imap_app_name'] = 'IMAP மற்றும் POP சர்வர்';
-$lang['imap_failed_logins'] = ', IMAP மின்னஞ்சல் உள்நுழைவுகள் தோல்வி';
-$lang['imap_imap'] = 'ஐஎம்ஏபி';
-$lang['imap_imaps'] = 'பாதுகாப்பான, IMAP';
+$lang['imap_app_description'] = 'IMAP மற்றும் POP சேவையகம் சேவையகத்துடன் இணைக்க மற்றும் மின்னஞ்சல் / மீட்டெடுப்பு மின்னஞ்சலை மின்னஞ்சல் வாடிக்கையாளர்களுக்கு (Thunderbird, மெயில், எவல்யூஷன், அவுட்லுக் / எக்ஸ்பிரஸ் முதலியன) ஒரு நிலையான சேவைகளை வழங்குகிறது.';
+$lang['imap_app_name'] = 'IMAP மற்றும் POP சேவையகம்';
+$lang['imap_failed_logins'] = 'IMAP அஞ்சல் தோல்வியடைந்த உள்நுழைவுகள்';
+$lang['imap_imap'] = 'IMAP ஐப்';
+$lang['imap_imaps'] = 'பாதுகாப்பான IMAP';
 $lang['imap_mail_service_invalid'] = 'மின்னஞ்சல் சேவை தவறானது.';
 $lang['imap_pop3'] = 'பாப்';
-$lang['imap_pop3s'] = 'பாதுகாப்பான, POP';
-$lang['imap_push_email'] = 'Push e-mail';
+$lang['imap_pop3s'] = 'பாதுகாப்பான POP';
+$lang['imap_push_email'] = 'மின்னஞ்சல் அனுப்பவும்';

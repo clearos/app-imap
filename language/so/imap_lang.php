a1670d97158ee4ac1e7b1b7afb67a3ce8f27d73c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:26:12 +0000

    Add new files and directories, commit changes, push changes

--- language/so/imap_lang.php
+++ language/so/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'server IMAP iyo POP waxay bixisaa go&#39;an caadiga ah ee adeegyada macaamiisha mail (Thunderbird, Mail, Evolution, Muuqaalka / Express iwm) in ay ku xidhmaan server iyo dib u soo ceshano / email bandhigay.';
+$lang['imap_app_description'] = 'IMAP iyo POP waxay bixiyaan adeegyo macaamiisha ah oo macaamiisha ah (Thunderbird, Mail, Evolution, Outlook / Express etc) si ay ugu xidhmaan serverka iyo email-ka soo-celinta / soo-celinta.';
 $lang['imap_app_name'] = 'IMAP iyo POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail fashilmay magacyo';
+$lang['imap_failed_logins'] = 'IMAP Boostada khatarta ah';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP Secure';
-$lang['imap_mail_service_invalid'] = 'adeegga Mail waa baadil.';
+$lang['imap_imaps'] = 'Badbaadada IMAP';
+$lang['imap_mail_service_invalid'] = 'Adeegga Boostada waa mid aan sharci ahayn.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP Secure';
-$lang['imap_push_email'] = 'Ka murugeeysan dib E-mail';
+$lang['imap_pop3s'] = 'Xaqiijin POP';
+$lang['imap_push_email'] = 'Riix E-mail';

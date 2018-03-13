56043bd29a74d90d49e6ebfa55d7ae64752c809a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:17:43 +0000

    Add new files and directories, commit changes, push changes

--- language/mt/imap_lang.php
+++ language/mt/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Il IMAP u POP server jipprovdi sett standard ta &#39;servizzi għall-klijenti tal-posta (Thunderbird, Mail, Evoluzzjoni, Outlook / Express eċċ) li jgħaqqdu l-server u jerġgħu jieħdu lura / email wiri.';
+$lang['imap_app_description'] = 'Is-server IMAP u POP jipprovdi sett standard ta &#39;servizzi għall-klijenti tal-posta (Thunderbird, Mail, Evolution, Outlook / Express, eċċ.) Biex jikkonnettjaw mas-server u jġibu / jaraw email.';
 $lang['imap_app_name'] = 'IMAP u POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail rnexxielhomx logins';
+$lang['imap_failed_logins'] = 'Biljetti tal-IMAP Fallew Logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP sikura';
-$lang['imap_mail_service_invalid'] = 'servizz tal-posta huwa invalidu.';
+$lang['imap_imaps'] = 'IMAP Sigura';
+$lang['imap_mail_service_invalid'] = 'Is-servizz tal-posta huwa invalidu.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP sikura';
+$lang['imap_pop3s'] = 'POP sikur';
 $lang['imap_push_email'] = 'Imbotta E-mail';

b9cf29de92db9e2d973068a1eee5d0f64801a009
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:01:33 +0000

    Add new files and directories, commit changes, push changes

--- language/ha/imap_lang.php
+++ language/ha/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'The IMAP da POP uwar garken samar da wani misali sa na ayyuka na mail abokan ciniki (Thunderbird, Mail, Evolution, Outlook / Express da dai sauransu) haɗi zuwa uwar garken da kuma mai da / nuni email.';
+$lang['imap_app_description'] = 'Saitunan IMAP da POP suna samar da saitattun ayyuka na abokan ciniki (Thunderbird, Mail, Evolution, Outlook / Express da dai sauransu.) Don haɗi zuwa uwar garke da kuma dawo da / imel na imel.';
 $lang['imap_app_name'] = 'IMAP da POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail nasarar logins';
+$lang['imap_failed_logins'] = 'An yi nasarar aikawa da IMAP Mail Logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail sabis ba daidai ba ne.';
+$lang['imap_imaps'] = 'Saƙon IMAP';
+$lang['imap_mail_service_invalid'] = 'Sabis ɗin sabis ɗin ba daidai ba ne.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_pop3s'] = 'POP mai tsaro';
 $lang['imap_push_email'] = 'Tura E-mail';

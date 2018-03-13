606e27c3e7bdfc77f6c7746975aeec5f938c184b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:21:26 +0000

    Add new files and directories, commit changes, push changes

--- language/pl/imap_lang.php
+++ language/pl/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Serwer IMAP i POP zapewnia standardowy zestaw usług dla klientów pocztowych (Thunderbird, Evolution, Mail, Outlook / Express, etc.), aby połączyć się z serwerem i pobrać / wyświetlacz e-mail.';
-$lang['imap_app_name'] = 'IMAP i POP Server';
-$lang['imap_failed_logins'] = 'IMAP Poczta Failed Logowania';
+$lang['imap_app_description'] = 'Serwer IMAP i POP udostępnia standardowy zestaw usług dla klientów pocztowych (Thunderbird, Mail, Evolution, Outlook / Express itp.), Aby połączyć się z serwerem i pobrać / wyświetlić wiadomość e-mail.';
+$lang['imap_app_name'] = 'Serwer IMAP i POP';
+$lang['imap_failed_logins'] = 'Logowanie IMAP nie powiodło się';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Bezpieczne IMAP';
-$lang['imap_mail_service_invalid'] = 'poczta jest nieprawidłowy.';
+$lang['imap_imaps'] = 'Bezpieczny IMAP';
+$lang['imap_mail_service_invalid'] = 'Usługa poczty jest nieprawidłowa.';
 $lang['imap_pop3'] = 'MUZYKA POP';
-$lang['imap_pop3s'] = 'Bezpieczne POP';
-$lang['imap_push_email'] = 'Push e-mail';
+$lang['imap_pop3s'] = 'Bezpieczny POP';
+$lang['imap_push_email'] = 'Wciśnij e-mail';

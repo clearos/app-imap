d533a4c77b1d555b74e61a7c59d511b994f003ff
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:20:11 +0000

    Add new files and directories, commit changes, push changes

--- language/pa/imap_lang.php
+++ language/pa/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP ਅਤੇ POP ਸਰਵਰ ਸਰਵਰ ਨਾਲ ਜੁੜਨ ਅਤੇ / ਡਿਸਪਲੇਅ ਈ-ਪ੍ਰਾਪਤ ਕਰਨ ਲਈ ਮੇਲ ਕਲਾਇਟ (ਥੰਡਰਬਰਡ, ਮੇਲ, ਈਵੇਲੂਸ਼ਨ, ਆਉਟਲੁੱਕ / ਐਕਸਪ੍ਰੈਸ ਆਦਿ) ਲਈ ਸੇਵਾ ਦਾ ਇੱਕ ਮਿਆਰੀ ਸੈੱਟ ਦਿੰਦਾ ਹੈ.';
+$lang['imap_app_description'] = 'IMAP ਅਤੇ POP ਸਰਵਰ ਮੇਲ ਕਲਾਇਟ (ਥੰਡਰਬਰਡ, ਮੇਲ, ਈਵੇਲੂਸ਼ਨ, ਆਉਟਲੁੱਕ / ਐਕਸਪ੍ਰੈਸ ਆਦਿ) ਲਈ ਸਰਵਰ ਨਾਲ ਜੁੜਨ ਅਤੇ / ਡਿਸਪਲੇ ਈਮੇਲ ਪ੍ਰਾਪਤ ਕਰਨ ਲਈ ਸੇਵਾਵਾਂ ਦਾ ਇੱਕ ਮਿਆਰੀ ਸੈੱਟ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
 $lang['imap_app_name'] = 'IMAP ਅਤੇ POP ਸਰਵਰ';
-$lang['imap_failed_logins'] = 'IMAP ਦੇ ਮੇਲ ਲਾਗਇਨ ਵਿੱਚ ਅਸਫਲ';
+$lang['imap_failed_logins'] = 'IMAP ਮੇਲ ਅਸਫਲ ਲਾਗਇਨ';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'ਸੁਰੱਖਿਅਤ IMAP';
-$lang['imap_mail_service_invalid'] = 'ਮੇਲ ਸੇਵਾ ਗਲਤ ਹੈ.';
+$lang['imap_mail_service_invalid'] = 'ਮੇਲ ਸੇਵਾ ਅਵੈਧ ਹੈ';
 $lang['imap_pop3'] = 'ਪੌਪ';
-$lang['imap_pop3s'] = 'ਸੁਰੱਖਿਅਤ POP';
-$lang['imap_push_email'] = 'ਪੁਸ਼ ਈ-ਮੇਲ';
+$lang['imap_pop3s'] = 'ਸਕਿਓਰ ਪੌਪ';
+$lang['imap_push_email'] = 'ਪੁੱਲ ਈ-ਮੇਲ';

ef8438b9ffe0763439d4e6240a2f68b1f913ddec
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:31:32 +0000

    Add new files and directories, commit changes, push changes

--- language/th/imap_lang.php
+++ language/th/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'เซิร์ฟเวอร์ IMAP และ POP ให้ชุดมาตรฐานของการบริการสำหรับลูกค้า mail (ธันเดอร์เบิร์ด, Mail, วิวัฒนาการของ Outlook / ด่วน ฯลฯ ) เพื่อเชื่อมต่อกับเซิร์ฟเวอร์และดึงอีเมล์ / จอแสดงผล';
-$lang['imap_app_name'] = 'IMAP และเซิร์ฟเวอร์ POP';
-$lang['imap_failed_logins'] = 'อีเมล IMAP ไม่สามารถเข้าสู่ระบบ';
+$lang['imap_app_description'] = 'เซิร์ฟเวอร์ IMAP และ POP มีชุดบริการมาตรฐานสำหรับไคลเอ็นต์อีเมล (Thunderbird, Mail, Evolution, Outlook / Express ฯลฯ ) เพื่อเชื่อมต่อกับเซิร์ฟเวอร์และดึงข้อมูล / แสดงอีเมล';
+$lang['imap_app_name'] = 'เซิร์ฟเวอร์ IMAP และ POP';
+$lang['imap_failed_logins'] = 'การลงชื่อเข้าใช้ IMAP Mail ล้มเหลว';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP การรักษาความปลอดภัย';
-$lang['imap_mail_service_invalid'] = 'บริการอีเมลไม่ถูกต้อง';
+$lang['imap_imaps'] = 'รักษาความปลอดภัย IMAP';
+$lang['imap_mail_service_invalid'] = 'บริการจดหมายไม่ถูกต้อง';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP การรักษาความปลอดภัย';
-$lang['imap_push_email'] = 'อีเมล Push';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Push E-mail';

3cfb739d4103632e86ad9f04e33d3759cacbabb7
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:10:12 +0000

    Add new files and directories, commit changes, push changes

--- language/ko/imap_lang.php
+++ language/ko/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = '가 IMAP와 POP 서버는 서버에 연결하고 / 디스플레이 이메일을 검색하는 메일 클라이언트 (썬더 버드, 메일, 진화, 아웃룩 / 익스프레스 등) 서비스의 표준 세트를 제공합니다.';
+$lang['imap_app_description'] = 'IMAP 및 POP 서버는 메일 클라이언트 (Thunderbird, Mail, Evolution, Outlook / Express 등)에 대한 표준 서비스 세트를 제공하여 서버에 연결하고 전자 메일을 검색 / 표시합니다.';
 $lang['imap_app_name'] = 'IMAP 및 POP 서버';
-$lang['imap_failed_logins'] = 'IMAP 메일 로그인 실패';
+$lang['imap_failed_logins'] = 'IMAP 메일 실패 로그인';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = '보안 IMAP';
-$lang['imap_mail_service_invalid'] = '메일 서비스가 잘못되었습니다.';
+$lang['imap_imaps'] = '안전한 IMAP';
+$lang['imap_mail_service_invalid'] = '메일 서비스가 유효하지 않습니다.';
 $lang['imap_pop3'] = '팝';
-$lang['imap_pop3s'] = '보안 POP';
+$lang['imap_pop3s'] = '안전한 POP';
 $lang['imap_push_email'] = '푸시 이메일';

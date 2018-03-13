a52670b65a80a50385b08dc843984d0f48bda8b6
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:55:21 +0000

    Add new files and directories, commit changes, push changes

--- language/da/imap_lang.php
+++ language/da/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP og POP-serveren giver et standard sæt af tjenester til mail-klienter (Thunderbird, Mail, Evolution, Outlook / Express etc.) til at oprette forbindelse til serveren og hente / display email.';
-$lang['imap_app_name'] = 'IMAP og POP-server';
-$lang['imap_failed_logins'] = 'IMAP Mail mislykkedes Logins';
+$lang['imap_app_description'] = 'IMAP- og POP-serveren leverer et standard sæt tjenester til postklienter (Thunderbird, Mail, Evolution, Outlook / Express osv.) For at oprette forbindelse til serveren og hente / vise e-mail.';
+$lang['imap_app_name'] = 'IMAP og POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail mislykkedes logins';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'Sikker IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail service er ugyldig.';
+$lang['imap_mail_service_invalid'] = 'Mail-tjenesten er ugyldig.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'Sikker POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_push_email'] = 'Skub e-mail';

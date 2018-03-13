71916c3b297657628a0943bcce64dd21c0dd60f6
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:31:02 +0000

    Add new files and directories, commit changes, push changes

--- language/tg/imap_lang.php
+++ language/tg/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Дар IMAP ва POP сервер таъмин маҷмӯи стандартии хизматрасонӣ барои муштариён почтаи (Thunderbird, Почта, Evolution, Outlook / Express ва ғайра) барои пайваст шудан ба сервер ва ҷустуҷӯи / почтаи кунед.';
+$lang['imap_app_description'] = 'Сервери IMAP ва POP маҷмӯи стандартҳои хидматрасони барои муштариёни почта (Thunderbird, Mail, Эволютсия, Outlook / Express ва ғайра) барои пайваст шудан ба сервер ва гирифтани почтаи электронӣ / намоиш дода мешаванд.';
 $lang['imap_app_name'] = 'IMAP ва POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail муяссар рамзњои';
+$lang['imap_failed_logins'] = 'IMAP почтаҳои қабулнашуда';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP Амн';
-$lang['imap_mail_service_invalid'] = 'Хадамоти Mail нодуруст аст.';
+$lang['imap_imaps'] = 'Нигоҳ доштани IMAP';
+$lang['imap_mail_service_invalid'] = 'Хизматрасонии почта беэътибор аст';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP Амн';
-$lang['imap_push_email'] = 'Push электронии';
+$lang['imap_pop3s'] = 'POP-и бехатар';
+$lang['imap_push_email'] = 'E-mail-ро пахш кунед';

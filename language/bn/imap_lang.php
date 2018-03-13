f6ea964f0d0ae60fb62a410df4983c7318f0bec5
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:49:48 +0000

    Add new files and directories, commit changes, push changes

--- language/bn/imap_lang.php
+++ language/bn/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP এবং POP সার্ভার সার্ভারের সাথে সংযোগ এবং / প্রদর্শন ইমেল পুনরুদ্ধার করতে মেইল ​​ক্লায়েন্ট (থান্ডারবার্ড, মেল, বিবর্তন, আউটলুক / এক্সপ্রেস ইত্যাদি) জন্য পরিষেবার একটি মান সেট প্রদান করে.';
+$lang['imap_app_description'] = 'IMAP এবং POP সার্ভার সার্ভারের সাথে সংযোগ স্থাপন এবং ইমেল প্রদর্শন / প্রদর্শন করার জন্য মেইল ​​ক্লায়েন্টদের জন্য একটি স্ট্যান্ডার্ড সেট পরিষেবা প্রদান করে (থান্ডারবার্ড, মেল, বিবর্তন, আউটলুক / এক্সপ্রেস ইত্যাদি)।';
 $lang['imap_app_name'] = 'IMAP এবং POP সার্ভার';
-$lang['imap_failed_logins'] = 'আইম্যাপ মেইল ​​লগইন ব্যর্থ';
-$lang['imap_imap'] = 'আইম্যাপ';
-$lang['imap_imaps'] = 'সিকিউর আইম্যাপ';
-$lang['imap_mail_service_invalid'] = 'মেল সেবা অবৈধ.';
-$lang['imap_pop3'] = 'পপ';
-$lang['imap_pop3s'] = 'সিকিউর পপ';
-$lang['imap_push_email'] = 'পুশ ই-মেইল';
+$lang['imap_failed_logins'] = 'IMAP মেইল ​​ব্যর্থ লগইন';
+$lang['imap_imap'] = 'IMAP এর';
+$lang['imap_imaps'] = 'নিরাপদ IMAP';
+$lang['imap_mail_service_invalid'] = 'মেল পরিষেবাটি অবৈধ';
+$lang['imap_pop3'] = 'POP এর';
+$lang['imap_pop3s'] = 'নিরাপদ POP';
+$lang['imap_push_email'] = 'ধাক্কা ই-মেইল';

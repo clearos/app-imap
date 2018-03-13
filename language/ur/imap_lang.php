169c93cbd24fbc051b0e26f37d3c284f3e9930fd
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:33:42 +0000

    Add new files and directories, commit changes, push changes

--- language/ur/imap_lang.php
+++ language/ur/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP اور POP سرور سرور سے رابطہ قائم کریں اور بازیافت / ڈسپلے ای میل پر میل کلائنٹس (تھنڈر برڈ، میل، ارتقاء، آؤٹ لک / ایکسپریس وغیرہ) کے لیے خدمات کا ایک معیاری سیٹ فراہم.';
+$lang['imap_app_description'] = 'IMAP اور POP سرور سرور سے منسلک کرنے اور ای میل کو ڈسپلے کرنے / ڈسپلے کرنے کے لئے میل کلائنٹس (تھنڈر بیڈ، میل، ارتقاء، آؤٹ لک / ایکسپریس وغیرہ) کیلئے معیاری سیٹ فراہم کرتا ہے.';
 $lang['imap_app_name'] = 'IMAP اور POP سرور';
-$lang['imap_failed_logins'] = 'IMAP میل لاگ ان ناکام ہوگیا';
+$lang['imap_failed_logins'] = 'IMAP میل ناکام لاگ ان';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'محفوظ IMAP';
 $lang['imap_mail_service_invalid'] = 'میل سروس غلط ہے.';
-$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3'] = 'پوپ';
 $lang['imap_pop3s'] = 'محفوظ POP';
-$lang['imap_push_email'] = 'پش ای میل';
+$lang['imap_push_email'] = 'ای میل پش';

e9161b8958d3d976f7a220c2bd0a05a1f212a8ae
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:05:10 +0000

    Add new files and directories, commit changes, push changes

--- language/ig/imap_lang.php
+++ language/ig/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'The IMAP na Pop nkesa na-enye a ọkọlọtọ set nke ọrụ maka mail ahịa (Thunderbird, Mail, Evolution, Outlook / Express wdg) jikọọ na ihe nkesa na weghachite / ngosi email.';
-$lang['imap_app_name'] = 'IMAP na Pop Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Okpu logins';
+$lang['imap_app_description'] = 'Netwọk IMAP na POP na-enye ọrụ setịpụrụ maka ndị ahịa mail (Thunderbird, Mail, Evolution, Outlook / Express wdg) iji jikọọ na nkesa na weghachite / gosipụtara ozi-e.';
+$lang['imap_app_name'] = 'IMAP na POP Server';
+$lang['imap_failed_logins'] = 'Ozi IMAP emeghị nke ọma';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail ozi bụ ghara ịdị irè.';
-$lang['imap_pop3'] = 'Pop';
-$lang['imap_pop3s'] = 'Secure Pop';
+$lang['imap_imaps'] = 'IMAP echekwa';
+$lang['imap_mail_service_invalid'] = 'Ozi na-ezighi ezi.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP siri ike';
 $lang['imap_push_email'] = 'Push E-mail';

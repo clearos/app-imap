54063e162917302d5b7e70b02766eea4b133df1b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:18:21 +0000

    Add new files and directories, commit changes, push changes

--- language/ne/imap_lang.php
+++ language/ne/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'यो IMAP र POP सर्भरले मेल ग्राहकहरु (थन्डरबर्ड, मेल, विकास, आउटलुक / एक्सप्रेस आदि) को लागि सेवा को एक मानक सेट सर्भर जडान र पुन: प्राप्त गर्न / प्रदर्शन इमेल गर्न प्रदान गर्दछ।';
+$lang['imap_app_description'] = 'IMAP र POP सर्भरले सर्वरसँग जडान गर्न र प्रदर्शन / डिस्प्ले इमेल प्रदर्शन गर्न मेल क्लाइन्टहरू (थन्डरबर्ड, मेल, इभोल्युसन, आउटलुक / एक्स्प्रेस आदि) को लागि मानक सेट सेवा प्रदान गर्दछ।';
 $lang['imap_app_name'] = 'IMAP र POP सर्भर';
-$lang['imap_failed_logins'] = 'IMAP मेल लगइनहरू असफल';
+$lang['imap_failed_logins'] = 'IMAP मेल असफल लगइनहरू';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'सुरक्षित IMAP';
+$lang['imap_imaps'] = 'IMAP सुरक्षित गर्नुहोस्';
 $lang['imap_mail_service_invalid'] = 'मेल सेवा अमान्य छ।';
-$lang['imap_pop3'] = 'पप';
-$lang['imap_pop3s'] = 'सुरक्षित POP';
-$lang['imap_push_email'] = 'पुस ई-मेल';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP सुरक्षित गर्नुहोस्';
+$lang['imap_push_email'] = 'ई-मेल पुष्ट गर्नुहोस्';

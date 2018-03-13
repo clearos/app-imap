ac9268a1b1e64146a2ca134e45fe5f2ae522de34
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:52:51 +0000

    Add new files and directories, commit changes, push changes

--- language/ny/imap_lang.php
+++ language/ny/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'The IMAP ndi Pop makina amapereka chitsanzo cha ntchito makasitomala makalata (Thunderbird, Mail, Evolution, Outlook / Express etc.) kulumikiza makina ndi kupeza / anasonyeza imelo.';
-$lang['imap_app_name'] = 'IMAP ndi Pop Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Chalephera Logins';
+$lang['imap_app_description'] = 'Seva ya IMAP ndi POP imapereka mndandanda wa mautumiki opempha makalata (Thunderbird, Mail, Evolution, Outlook / Express etc.) kuti agwirizane ndi seva ndi kulandila imelo.';
+$lang['imap_app_name'] = 'IMAP ndi POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail inalephera kulowa';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'otetezeka IMAP';
-$lang['imap_mail_service_invalid'] = 'utumiki Mail ndi opanda pake.';
-$lang['imap_pop3'] = 'Pop';
-$lang['imap_pop3s'] = 'otetezeka Pop';
-$lang['imap_push_email'] = 'Kankhani E-mail';
+$lang['imap_imaps'] = 'IMAP yotetezeka';
+$lang['imap_mail_service_invalid'] = 'Utumiki wa makalata ndi wosayenera.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP Yotsimikizika';
+$lang['imap_push_email'] = 'Sungani E-mail';

0786817f2990ddfdbfe7942860335ceb3b1c7fb4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:34:33 +0000

    Add new files and directories, commit changes, push changes

--- language/uz/imap_lang.php
+++ language/uz/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP va POP server serverga ulanish va / displey, elektron pochta qabul qilish uchun pochta mijozlari (Thunderbird, Mail, evolyutsiya, Outlook / Express va hokazo) uchun xizmatlar standart majmuini beradi.';
-$lang['imap_app_name'] = 'IMAP va POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail sessiyalari bo&#39;lmadi';
+$lang['imap_app_description'] = 'IMAP va POP server serverga ulanish va elektron pochta xabarlarini qabul qilish uchun pochta mijozlariga (Thunderbird, Mail, Evolution, Outlook / Express va boshqalar) standart xizmatlarni taqdim etadi.';
+$lang['imap_app_name'] = 'IMAP va POP serveri';
+$lang['imap_failed_logins'] = 'IMAP xatosi amalga oshmadi';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Xavfsiz IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail xizmati haqiqiy emas.';
+$lang['imap_imaps'] = 'IMAP xavfsizligi';
+$lang['imap_mail_service_invalid'] = 'Pochta xizmati noto&#39;g&#39;ri.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'Xavfsiz POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_push_email'] = 'E-mail ni bosing';

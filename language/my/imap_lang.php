142c944db77cc92f189df6dbdfe9143bd4ea19fe
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:51:35 +0000

    Add new files and directories, commit changes, push changes

--- language/my/imap_lang.php
+++ language/my/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'အဆိုပါ IMAP ကိုနှင့် POP ဆာဗာဆာဗာကိုချိတ်ဆက်နှင့် / display ကိုအီးမေးလ်က retrieve မှမေးလ် client များအတွက်န်ဆောင်မှုများဟာ standard set ကို (Thunderbird ကို Mail, Evolution, Outlook နဲ့ / Express ကို etc) သည်။';
-$lang['imap_app_name'] = 'IMAP ကိုနဲ့ POP Server ကို';
-$lang['imap_failed_logins'] = 'IMAP ကိုမေးလ် Login မအောင်မြင်ခဲ့ပါ';
+$lang['imap_app_description'] = 'အဆိုပါ IMAP ကိုနှင့် POP ဆာဗာဆာဗာကိုချိတ်ဆက်နှင့် / Display ကိုအီးမေးလ်က retrieve မှမေးလ် client များအတွက်ဝန်ဆောင်မှုများကိုစံသတ်မှတ်ချက် (Thunderbird ကို, Mail, Evolution, Outlook / Express ကို etc) သည်။';
+$lang['imap_app_name'] = 'IMAP ကိုနှင့် POP ဆာဗာ';
+$lang['imap_failed_logins'] = 'IMAP ကိုမေးလ် login Failed';
 $lang['imap_imap'] = 'IMAP ကို';
 $lang['imap_imaps'] = 'လုံလုံခြုံခြုံ IMAP ကို';
-$lang['imap_mail_service_invalid'] = 'မေးလ်ဝန်ဆောင်မှုမမှန်ကန်ဘူး။';
+$lang['imap_mail_service_invalid'] = 'မေးလ်ဝန်ဆောင်မှုသည်မှားနေပါသည်။';
 $lang['imap_pop3'] = 'POP ကို';
 $lang['imap_pop3s'] = 'လုံလုံခြုံခြုံ POP ကို';
 $lang['imap_push_email'] = 'Push ကို E-mail ကို';

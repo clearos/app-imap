9c705ea773a01ac415cab5edb355f12df26c1634
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:07:42 +0000

    Add new files and directories, commit changes, push changes

--- language/ka/imap_lang.php
+++ language/ka/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP და POP სერვერზე უზრუნველყოფს სტანდარტულ მომსახურებას ფოსტა კლიენტებს (Thunderbird, Mail, Evolution, Outlook / Express და ა.შ.), სერვერთან დაკავშირება და ახლიდან / display ელ.';
+$lang['imap_app_description'] = 'IMAP და POP სერვერი უზრუნველყოფს საფოსტო კლიენტებისთვის (Thunderbird, Mail, Evolution, Outlook / Express და ა.შ.) სერვისების სტანდარტული კომპლექტი სერვერთან დაკავშირებას და დაბრუნების / გამოტანის ელ.';
 $lang['imap_app_name'] = 'IMAP და POP სერვერი';
-$lang['imap_failed_logins'] = 'IMAP Mail ვერ Logins';
+$lang['imap_failed_logins'] = 'IMAP ფოსტა ჩაიშალა შეცდომები';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'უსაფრთხო IMAP';
-$lang['imap_mail_service_invalid'] = 'გვერდის მომსახურება არასწორია.';
+$lang['imap_mail_service_invalid'] = 'ფოსტის სერვისი არასწორია.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'უსაფრთხო POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_push_email'] = 'დააყენე ელ.ფოსტა';

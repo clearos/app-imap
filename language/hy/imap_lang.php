5863bb3e2f1d9927deb5a134ca0ab14647e48faa
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:47:17 +0000

    Add new files and directories, commit changes, push changes

--- language/hy/imap_lang.php
+++ language/hy/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'The IMAP եւ POP սերվերի ապահովում է ստանդարտ շարք ծառայությունների համար փոստի հաճախորդների (Thunderbird, Mail, էվոլյուցիան, Outlook / Express եւ այլն) միացնել սերվերի եւ վերստանալ / ցուցադրման նամակ.';
+$lang['imap_app_description'] = 'IMAP- ի եւ POP- ի սերվերը ապահովում է սերվերին միանալու եւ էլ.փոստի հասցեին միանալու համար փոստի հաճախորդների համար ստանդարտ ծառայություն (Thunderbird, Mail, Evolution, Outlook / Express եւ այլն):';
 $lang['imap_app_name'] = 'IMAP եւ POP Server';
-$lang['imap_failed_logins'] = 'IMAP Փոստ ձախողված';
-$lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Փոստի ծառայություն է անվավեր:';
-$lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Secure POP';
-$lang['imap_push_email'] = 'Հրել E-mail';
+$lang['imap_failed_logins'] = 'IMAP Փոստը չհաջողվեց մուտք գործել';
+$lang['imap_imap'] = 'IMAP- ը';
+$lang['imap_imaps'] = 'Ապահով IMAP- ը';
+$lang['imap_mail_service_invalid'] = 'Փոստի ծառայությունը անվավեր է:';
+$lang['imap_pop3'] = 'POP- ը';
+$lang['imap_pop3s'] = 'Անվտանգ POP- ը';
+$lang['imap_push_email'] = 'Ուղարկել էլ. Փոստը';

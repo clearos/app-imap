35850e8aa7891790048c77b3af8fe5f1e5479e41
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:09:36 +0000

    Add new files and directories, commit changes, push changes

--- language/kn/imap_lang.php
+++ language/kn/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್ ಸರ್ವರ್ ಸಂಪರ್ಕ ಮತ್ತು / ಪ್ರದರ್ಶನ ಇಮೇಲ್ ಹಿಂಪಡೆಯಲು ಮೇಲ್ ಕ್ಲೈಂಟ್ಗಳು (ತಂಡರ್, ಮೇಲ್, ವಿಕಸನ, ಔಟ್ಲುಕ್ / ಎಕ್ಸ್ಪ್ರೆಸ್ ಇತ್ಯಾದಿ) ಸೇವೆಗಳ ಒಂದು ಸಾಮಾನ್ಯ ಸೆಟ್ ಒದಗಿಸುತ್ತದೆ.';
-$lang['imap_app_name'] = 'IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್';
-$lang['imap_failed_logins'] = 'IMAP ಮೇಲ್ ವಿಫಲವಾಗಿದೆ ಲಾಗಿನ್ನುಗಳು';
-$lang['imap_imap'] = 'IMAP ಅನ್ನು';
-$lang['imap_imaps'] = 'ಸುರಕ್ಷಿತ IMAP ಅನ್ನು';
-$lang['imap_mail_service_invalid'] = 'ಮೇಲ್ ಸೇವೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
-$lang['imap_pop3'] = 'ಪಾಪ್';
-$lang['imap_pop3s'] = 'ಸುರಕ್ಷಿತ ಪಾಪ್';
-$lang['imap_push_email'] = 'ಪುಷ್ ಮೇಲ್';
+$lang['imap_app_description'] = 'IMAP ಮತ್ತು POP ಸರ್ವರ್ ಸರ್ವರ್ಗಳಿಗೆ ಸಂಪರ್ಕ ಸಾಧಿಸಲು ಮತ್ತು ಮರುಪ್ರದರ್ಶನ / ಪ್ರದರ್ಶನ ಇಮೇಲ್ಗೆ ಮೇಲ್ ಕ್ಲೈಂಟ್ಗಳಿಗೆ (ಥಂಡರ್ಬರ್ಡ್, ಮೇಲ್, ಎವಲ್ಯೂಷನ್, ಔಟ್ಲುಕ್ / ಎಕ್ಸ್ಪ್ರೆಸ್ ಇತ್ಯಾದಿ) ಒಂದು ಪ್ರಮಾಣಿತ ಸೇವೆಗಳನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['imap_app_name'] = 'IMAP ಮತ್ತು POP ಸರ್ವರ್';
+$lang['imap_failed_logins'] = 'IMAP ಮೇಲ್ ಲಾಗಿನ್ಸ್ ವಿಫಲವಾಗಿದೆ';
+$lang['imap_imap'] = 'IMAP';
+$lang['imap_imaps'] = 'ಸುರಕ್ಷಿತ IMAP';
+$lang['imap_mail_service_invalid'] = 'ಮೇಲ್ ಸೇವೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'ಸುರಕ್ಷಿತ POP';
+$lang['imap_push_email'] = 'ಇ-ಮೇಲ್ ಅನ್ನು ಪುಶ್ ಮಾಡಿ';

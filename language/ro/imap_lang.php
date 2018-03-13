960026c6b31d06f802caed41e7855e186408ab73
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:22:41 +0000

    Add new files and directories, commit changes, push changes

--- language/ro/imap_lang.php
+++ language/ro/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Server IMAP și POP oferă un set standard de servicii pentru clienții de poștă electronică (Thunderbird, Mail, Evolution, Outlook / Express etc.) pentru a se conecta la server și de a prelua / e-mail de afișare.';
+$lang['imap_app_description'] = 'Serverul IMAP și POP oferă un set standard de servicii pentru clienții de poștă electronică (Thunderbird, Mail, Evolution, Outlook / Express etc.) pentru a vă conecta la server și pentru a prelua / afișa e-mailuri.';
 $lang['imap_app_name'] = 'IMAP și POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail nu a reușit Conectări';
+$lang['imap_failed_logins'] = 'Mesajele nereușite pentru mesajele IMAP';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP securizat';
-$lang['imap_mail_service_invalid'] = 'Serviciul Mail este nevalid.';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'Serviciul de poștă electronică este nevalid.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP securizat;';
-$lang['imap_push_email'] = 'Push e-mail';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'Apăsați E-mail';

11375c296919daa26d918c9d14ae67e2413e38a2
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:25:45 +0000

    Add new files and directories, commit changes, push changes

--- language/sl/imap_lang.php
+++ language/sl/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Strežnik IMAP in POP zagotavlja standardni nabor storitev za poštne odjemalce (Thunderbird, Mail, Evolution, Outlook / Express itd) za povezavo s strežnikom in pridobivanje / zaslon e-pošto.';
+$lang['imap_app_description'] = 'Strežnik IMAP in POP zagotavlja standarden nabor storitev za poštne odjemalce (Thunderbird, Mail, Evolution, Outlook / Express itd.) Za povezavo s strežnikom in priklic / prikaz e-pošte.';
 $lang['imap_app_name'] = 'IMAP in POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail uspelo podatki za prijavo';
+$lang['imap_failed_logins'] = 'IMAP Pohištvo ni uspelo';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Poštnih storitev je neveljavna.';
+$lang['imap_imaps'] = 'Varno IMAP';
+$lang['imap_mail_service_invalid'] = 'Poštna storitev ni veljavna.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'Secure POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_push_email'] = 'Potisnite e-pošto';

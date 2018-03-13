67b12d599c65f3d70ec17cf59d6ef27d3c1f121c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:20:49 +0000

    Add new files and directories, commit changes, push changes

--- language/fa/imap_lang.php
+++ language/fa/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP و POP سرور فراهم می کند یک مجموعه استاندارد از خدمات برای مشتریان پست الکترونیکی (تاندربرد، ایمیل، تکامل، چشم انداز / اکسپرس و غیره) برای اتصال به سرور و بازیابی / ایمیل ها.';
+$lang['imap_app_description'] = 'سرور IMAP و POP یک سرویس استاندارد برای سرویس گیرندگان ایمیل (Thunderbird، Mail، Evolution، Outlook / Express و غیره) را برای اتصال به سرور و بازیابی / نمایش ایمیل فراهم می کند.';
 $lang['imap_app_name'] = 'IMAP و POP سرور';
-$lang['imap_failed_logins'] = 'IMAP ایمیل نشد باری';
+$lang['imap_failed_logins'] = 'ورودی IMAP ایمیل خراب شد';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP امن';
-$lang['imap_mail_service_invalid'] = 'سرویس ایمیل نامعتبر است.';
+$lang['imap_imaps'] = 'ایمن ایمن';
+$lang['imap_mail_service_invalid'] = 'سرویس پستی نامعتبر است';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'POP امن';
 $lang['imap_push_email'] = 'فشار پست الکترونیکی';

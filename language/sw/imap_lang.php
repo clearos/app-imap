df2773910d6b6be31a0ab597a39069b9e31de4f5
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:28:38 +0000

    Add new files and directories, commit changes, push changes

--- language/sw/imap_lang.php
+++ language/sw/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP na POP server hutoa kiwango seti ya huduma kwa wateja pepe (Thunderbird, Mail, Evolution, Outlook / Express nk) kuungana na server na retrieve / kuonyesha email.';
+$lang['imap_app_description'] = 'Seva ya IMAP na POP hutoa seti ya huduma ya kawaida kwa wateja wa barua (Thunderbird, Mail, Evolution, Outlook / Express nk) kuunganisha kwenye seva na kupata barua pepe.';
 $lang['imap_app_name'] = 'IMAP na POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Imeshindwa Logins';
+$lang['imap_failed_logins'] = 'Barua ya IMAP Imeshindwa Ingia';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'salama IMAP';
-$lang['imap_mail_service_invalid'] = 'huduma ya barua ni batili.';
+$lang['imap_imaps'] = 'IMAP salama';
+$lang['imap_mail_service_invalid'] = 'Huduma ya barua pepe ni batili.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'salama POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_pop3s'] = 'POP salama';
+$lang['imap_push_email'] = 'Pushisha barua pepe';

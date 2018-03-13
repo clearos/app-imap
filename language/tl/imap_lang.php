d9b18f4cec064f3b33deee828794f767cc316204
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:31:59 +0000

    Add new files and directories, commit changes, push changes

--- language/tl/imap_lang.php
+++ language/tl/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Ang IMAP at POP server ay nagbibigay ng isang standard na hanay ng mga serbisyo para sa mga mail client (Thunderbird, Mail, Evolution, Outlook / Express atbp) upang kumonekta sa server at kunin / display email.';
+$lang['imap_app_description'] = 'Ang server ng IMAP at POP ay nagbibigay ng isang standard na hanay ng mga serbisyo para sa mga mail client (Thunderbird, Mail, Ebolusyon, Outlook / Express atbp.) Upang kumonekta sa server at kunin / magpakita ng email.';
 $lang['imap_app_name'] = 'IMAP at POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Nabigong Pag-login';
+$lang['imap_failed_logins'] = 'Nabigong Mga Pag-login ng IMAP Mail';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail serbisyo ay hindi wasto.';
+$lang['imap_mail_service_invalid'] = 'Di-wasto ang serbisyo sa mail.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'Secure POP';
 $lang['imap_push_email'] = 'Push E-mail';

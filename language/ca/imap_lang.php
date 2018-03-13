04b7697204da988c5f134d521a20ad1f745374f1
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:52:26 +0000

    Add new files and directories, commit changes, push changes

--- language/ca/imap_lang.php
+++ language/ca/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'El servidor IMAP i POP proporciona un conjunt estàndard de serveis per als clients de correu (Thunderbird, Mail, Evolution, Outlook / exprés, etc.) per connectar-se al servidor i recuperar el correu electrònic / pantalla.';
-$lang['imap_app_name'] = 'IMAP i POP del servidor';
-$lang['imap_failed_logins'] = 'IMAP de correu d&#39;error inicis de sessió';
+$lang['imap_app_description'] = 'El servidor IMAP i POP proporciona un conjunt estàndard de serveis per als clients de correu (Thunderbird, Mail, Evolution, Outlook / Express, etc.) per connectar-se al servidor i recuperar / visualitzar el correu electrònic.';
+$lang['imap_app_name'] = 'Servidor IMAP i POP';
+$lang['imap_failed_logins'] = 'S&#39;ha produït un error en iniciar sessió amb el correu IMAP';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'IMAP segur';
 $lang['imap_mail_service_invalid'] = 'El servei de correu no és vàlid.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'POP segur';
-$lang['imap_push_email'] = 'Push correu electrònic';
+$lang['imap_push_email'] = 'Push E-mail';

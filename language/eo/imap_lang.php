9023355a57b0faedeae67db94e7d8360c815ddaf
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:56:11 +0000

    Add new files and directories, commit changes, push changes

--- language/eo/imap_lang.php
+++ language/eo/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'La IMAP kaj POPO servilo disponigas norman aron de servoj por klientoj de poŝto (Thunderbird, Poŝto, Evolution, Outlook / Express ktp) por konekti al la servilo kaj elsxuti / montriĝo retpoŝto.';
-$lang['imap_app_name'] = 'IMAP kaj POPO Servilo';
-$lang['imap_failed_logins'] = 'IMAP Poŝto Malsukcesaj logins';
+$lang['imap_app_description'] = 'La servilo de IMAP kaj POP provizas norman aron da servoj por retpoŝtaj klientoj (Thunderbird, Poŝto, Evoluado, Outlook / Express ktp) por konekti al la servilo kaj rekuperi / montri retpoŝton.';
+$lang['imap_app_name'] = 'IMAP kaj POP-servilo';
+$lang['imap_failed_logins'] = 'IMAP Poŝto Malsukcesis Ensalutojn';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'sekura IMAP';
-$lang['imap_mail_service_invalid'] = 'Retpoŝtservon estas nevalida.';
+$lang['imap_imaps'] = 'Sekura IMAP';
+$lang['imap_mail_service_invalid'] = 'Poŝto servo nevalidas.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'sekura POP';
-$lang['imap_push_email'] = 'Puŝo Retpoŝto';
+$lang['imap_pop3s'] = 'Sekura POP';
+$lang['imap_push_email'] = 'Forpuŝu retpoŝton';

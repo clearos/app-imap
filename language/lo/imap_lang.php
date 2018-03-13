f17a59b29341d8ad0ac03075c9b0f11bcfcd31dd
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:11:28 +0000

    Add new files and directories, commit changes, push changes

--- language/lo/imap_lang.php
+++ language/lo/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'ເຄື່ອງແມ່ຂ່າຍຂອງ IMAP ແລະ POP ໃຫ້ຊຸດມາດຕະຖານຂອງການບໍລິການສໍາລັບລູກຄ້າ mail (Thunderbird, Mail, Evolution, ການຄາດຄະເນ / ການສະແດງອອກແລະອື່ນໆ) ເພື່ອເຊື່ອມຕໍ່ກັບເຄື່ອງແມ່ຂ່າຍແລະດຶງ email / ການສະແດງ.';
+$lang['imap_app_description'] = 'ເຄື່ອງແມ່ຂ່າຍ IMAP ແລະ POP ໃຫ້ບໍລິການມາດຕະຖານສໍາລັບລູກຄ້າອີເມວ (Thunderbird, Mail, Evolution, Outlook / Express, ແລະອື່ນໆ) ເພື່ອເຊື່ອມຕໍ່ກັບເຄື່ອງແມ່ຂ່າຍແລະຄົ້ນຫາ / ສະແດງອີເມວ.';
 $lang['imap_app_name'] = 'IMAP ແລະ POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail ບໍ່ສາມາດເຂົ້າສູ່ລະບົບ';
+$lang['imap_failed_logins'] = 'IMAP Mail Failed Logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'IMAP ທີ່ປອດໄພ';
-$lang['imap_mail_service_invalid'] = 'ການບໍລິການ Mail ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['imap_imaps'] = 'Secure IMAP';
+$lang['imap_mail_service_invalid'] = 'ບໍລິການທາງອີເມວບໍ່ຖືກຕ້ອງ.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'POP ທີ່ປອດໄພ';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_push_email'] = 'ກົດ E -mail';

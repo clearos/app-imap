3e0631617207ae5f632887f4396e829dd3878083
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:07:04 +0000

    Add new files and directories, commit changes, push changes

--- language/ja/imap_lang.php
+++ language/ja/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAPとPOPサーバは、サーバに接続し、/表示電子メールを取得するために、メールクライアント（Thunderbirdの、メール、進化は、Outlook / Expressの等）のためのサービスの標準セットを提供します。';
+$lang['imap_app_description'] = 'IMAPとPOPサーバーは、メールクライアント（Thunderbird、Mail、Evolution、Outlook / Expressなど）の標準的な一連のサービスを提供し、サーバーに接続して電子メールを取得/表示します。';
 $lang['imap_app_name'] = 'IMAPとPOPサーバー';
-$lang['imap_failed_logins'] = 'IMAPメールは、ログインの失敗しました';
+$lang['imap_failed_logins'] = 'IMAPメールに失敗したログイン';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'セキュアIMAP';
 $lang['imap_mail_service_invalid'] = 'メールサービスが無効です。';
 $lang['imap_pop3'] = 'ポップ';
-$lang['imap_pop3s'] = 'セキュアPOP';
-$lang['imap_push_email'] = 'プッシュメール';
+$lang['imap_pop3s'] = 'セキュアなPOP';
+$lang['imap_push_email'] = 'プッシュEメール';

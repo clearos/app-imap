be7f5b121dd6453214e899bcd39317bf85776fc0
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:08:21 +0000

    Add new files and directories, commit changes, push changes

--- language/kk/imap_lang.php
+++ language/kk/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP және POP сервер серверге қосылу және / дисплей электрондық пошта шығарып алуға пошта клиенттер үшін қызметтердің стандартты жиынтығы (Thunderbird, Пошта, эволюция Outlook / Express және т.б.) қамтамасыз етеді.';
-$lang['imap_app_name'] = 'IMAP және POP сервер';
-$lang['imap_failed_logins'] = 'IMAP пошта Логин жаңылысы';
+$lang['imap_app_description'] = 'IMAP және POP сервері серверге қосылу және электрондық поштаны алу / шығару үшін пошталық клиенттерге (Thunderbird, Mail, Evolution, Outlook / Express және т.б.) стандартты қызметтер жиынтығын ұсынады.';
+$lang['imap_app_name'] = 'IMAP және POP сервері';
+$lang['imap_failed_logins'] = 'IMAP поштасы сәтсіз аяқталды';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Қауіпсіз IMAP';
-$lang['imap_mail_service_invalid'] = 'Пошта қызметі жарамсыз болып табылады.';
+$lang['imap_imaps'] = 'IMAP қауіпсіздігін қамтамасыз ету';
+$lang['imap_mail_service_invalid'] = 'Пошта қызметі жарамсыз.';
 $lang['imap_pop3'] = 'ПОП';
 $lang['imap_pop3s'] = 'Қауіпсіз POP';
-$lang['imap_push_email'] = 'Push Электрондық пошта';
+$lang['imap_push_email'] = 'Электрондық поштаны басу';

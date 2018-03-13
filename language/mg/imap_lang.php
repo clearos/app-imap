1b72a44e7a8bf81e215a1d36f205f5771c846696
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:13:23 +0000

    Add new files and directories, commit changes, push changes

--- language/mg/imap_lang.php
+++ language/mg/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Ny IMAP sy ny POP mpizara manome ny fitsipika napetraka ny asa ho namany Sary mpanjifa (Thunderbird, Mail, Evolution, Outlook / Express sns) mba mifandray amin&#39;ny lohamilina ary retrieve / miseho mailaka.';
-$lang['imap_app_name'] = 'IMAP sy ny POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Tsy nahomby Logins';
+$lang['imap_app_description'] = 'Ny mpizara IMAP sy POP dia manome serivisy serivisy ho an&#39;ny mpanjifa mailaka (Thunderbird, Mail, Evolution, Outlook / Express sns.) Mba hifandray amin&#39;ny server ary hizaha mailaka.';
+$lang['imap_app_name'] = 'IMAP sy POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail Failed Logins';
 $lang['imap_imap'] = 'IMAP';
 $lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail fanompoana dia tsy manan-kery.';
+$lang['imap_mail_service_invalid'] = 'Ny serivisy mailaka dia tsy manan-kery.';
 $lang['imap_pop3'] = 'POP';
 $lang['imap_pop3s'] = 'Secure POP';
-$lang['imap_push_email'] = 'Manosika E-mail';
+$lang['imap_push_email'] = 'Push E-mail';

c9316cceeb33f57ef51e58a1ce9460992d37ecda
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:54:54 +0000

    Add new files and directories, commit changes, push changes

--- language/cs/imap_lang.php
+++ language/cs/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP a POP server poskytuje standardní sadu služeb pro mailových klientů (Thunderbird, Mail, Evolution Outlook / Express podobně) pro připojení k serveru a vybírat poštu / displeje.';
-$lang['imap_app_name'] = 'IMAP a POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail se nezdařilo Logins';
+$lang['imap_app_description'] = 'Server IMAP a POP poskytuje standardní soubor služeb pro poštovní klienty (Thunderbird, Mail, Evolution, Outlook / Express atd.) Pro připojení k serveru a načítání / zobrazení e-mailů.';
+$lang['imap_app_name'] = 'IMAP a POP server';
+$lang['imap_failed_logins'] = 'Nepodařilo se přihlásit protokoly IMAP';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mailová služba je neplatný.';
+$lang['imap_imaps'] = 'Zabezpečte IMAP';
+$lang['imap_mail_service_invalid'] = 'Služba poštovní služby je neplatná.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_pop3s'] = 'Zabezpečení protokolu POP';
 $lang['imap_push_email'] = 'Push E-mail';

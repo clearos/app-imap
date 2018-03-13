e04c4efc046d4c9a9fe76dbbb4570a749bd23e01
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:26:42 +0000

    Add new files and directories, commit changes, push changes

--- language/st/imap_lang.php
+++ language/st/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'The IMAP le Pop seva e fana ka molao-motheo sete ya ditshebeletso tsa bareki poso (Thunderbird, Mail, ho Iphetola ha Lintho, Outlook / Express joalo-joalo) ho hokela seva &#39;me lata / ne a bonahatse imeile.';
-$lang['imap_app_name'] = 'IMAP le Pop Server';
-$lang['imap_failed_logins'] = 'IMAP Mail o ile a hlōleha Logins';
+$lang['imap_app_description'] = 'Sesepa sa IMAP le POP se fana ka setebeletso se tloaelehileng sa litšebeletso tsa mangolo (Thunderbird, Mail, Evolution, Outlook / Express joalo-joalo) ho ikopanya le seva le ho fumana melaetsa ea imeile.';
+$lang['imap_app_name'] = 'IMAP le POP Server';
+$lang['imap_failed_logins'] = 'IMAP Mail e hlōlehile ho kena';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'se sireletsehileng IMAP';
-$lang['imap_mail_service_invalid'] = 'tšebeletso ea Mail ha o sebetse.';
-$lang['imap_pop3'] = 'Pop';
-$lang['imap_pop3s'] = 'se sireletsehileng Pop';
-$lang['imap_push_email'] = 'Lekhalo pakeng E-mail';
+$lang['imap_imaps'] = 'IMAP e bolokehileng';
+$lang['imap_mail_service_invalid'] = 'Tšebeletso ea poso ha e na thuso.';
+$lang['imap_pop3'] = 'POP';
+$lang['imap_pop3s'] = 'POP e sireletsehileng';
+$lang['imap_push_email'] = 'Sebelisa E-mail';

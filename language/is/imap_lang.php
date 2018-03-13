f04112d984f88e8a7b5a21a1dd87f5228862fede
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:05:48 +0000

    Add new files and directories, commit changes, push changes

--- language/is/imap_lang.php
+++ language/is/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'The IMAP og POP þjóninum veitir venjulegu setja af þjónustu fyrir póstforrit (Thunderbird, Mail, Evolution, Outlook / Express osfrv) til að tengjast við miðlara og sótt / sýna tölvupósti.';
+$lang['imap_app_description'] = 'IMAP- og POP-þjónninn býður upp á staðlaða þjónustuþjónustu fyrir póstþjóna (Thunderbird, Mail, Evolution, Outlook / Express o.fl.) til að tengjast miðlara og sækja / birta tölvupóst.';
 $lang['imap_app_name'] = 'IMAP og POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail mistókst Innskráningar';
+$lang['imap_failed_logins'] = 'IMAP Mail Mistókst Logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Póstþjónustu er ógilt.';
+$lang['imap_imaps'] = 'Öruggt IMAP';
+$lang['imap_mail_service_invalid'] = 'Póstþjónusta er ógild.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Secure POP';
-$lang['imap_push_email'] = 'Ýta E-mail';
+$lang['imap_pop3s'] = 'Öruggur POP';
+$lang['imap_push_email'] = 'Ýttu á tölvupóst';

1f4fd55b879e7b9e3166973b9eba51930142594d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:45:15 +0000

    Add new files and directories, commit changes, push changes

--- language/af/imap_lang.php
+++ language/af/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Die IMAP en POP-bediener bied &#39;n standaard stel van dienste vir e-pos kliënte (Thunderbird, Mail, Evolution, Outlook / Express ens) om toegang tot die bediener en haal / vertoning e-pos.';
-$lang['imap_app_name'] = 'IMAP en POP Server';
-$lang['imap_failed_logins'] = 'IMAP-pos het misluk Logins';
+$lang['imap_app_description'] = 'Die IMAP- en POP-bediener bied &#39;n standaard stel dienste vir poskliënte (Thunderbird, Mail, Evolution, Outlook / Express, ens.) Om aan die bediener te koppel en e-pos te kry / te vertoon.';
+$lang['imap_app_name'] = 'IMAP en POP-bediener';
+$lang['imap_failed_logins'] = 'IMAP-pos misluk logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'veilige IMAP';
+$lang['imap_imaps'] = 'Veilige IMAP';
 $lang['imap_mail_service_invalid'] = 'Posdiens is ongeldig.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'veilige POP';
-$lang['imap_push_email'] = 'Push E-pos';
+$lang['imap_pop3s'] = 'Veilige POP';
+$lang['imap_push_email'] = 'Druk E-pos';

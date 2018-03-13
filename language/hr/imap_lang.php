1a17cbc6e6c0e03f0cf54cb5f7c87552811ee20b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 21:54:06 +0000

    Add new files and directories, commit changes, push changes

--- language/hr/imap_lang.php
+++ language/hr/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP i POP poslužitelj pruža standardni skup usluga za klijenta e-pošte (Thunderbird, Mail, evolucije Outlook / Express) kako bi se povezali s poslužiteljem i popravljanje / prikaz e-pošte.';
-$lang['imap_app_name'] = 'IMAP i POP poslužitelja';
-$lang['imap_failed_logins'] = 'IMAP e-pošte nije uspjelo Prijave';
+$lang['imap_app_description'] = 'IMAP i POP poslužitelj pruža standardni skup usluga za klijente e-pošte (Thunderbird, Mail, Evolution, Outlook / Express i sl.) Za povezivanje s poslužiteljem i dohvaćanje / prikazivanje e-pošte.';
+$lang['imap_app_name'] = 'IMAP i POP poslužitelj';
+$lang['imap_failed_logins'] = 'Prijava IMAP-a nije uspjela';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Sigurna IMAP';
-$lang['imap_mail_service_invalid'] = 'Pošte nije valjana.';
+$lang['imap_imaps'] = 'Sigurno IMAP';
+$lang['imap_mail_service_invalid'] = 'Usluga e-pošte nije valjana.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Sigurna POP';
-$lang['imap_push_email'] = 'Push E-mail';
+$lang['imap_pop3s'] = 'Sigurno POP';
+$lang['imap_push_email'] = 'Pritisnite E-poštu';

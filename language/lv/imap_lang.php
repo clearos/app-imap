98debb8c4dbdf587b2b336eb52d0f8c753165c7e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:12:43 +0000

    Add new files and directories, commit changes, push changes

--- language/lv/imap_lang.php
+++ language/lv/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'IMAP un POP serveris nodrošina standarta pakalpojumu kopumu par pasta klientiem (Thunderbird, Mail, Evolution, Outlook / Express uc), lai izveidotu savienojumu ar serveri un paņemt / displeja e-pastu.';
-$lang['imap_app_name'] = 'IMAP un POP Server';
-$lang['imap_failed_logins'] = 'IMAP pasta Neizdevās Logins';
+$lang['imap_app_description'] = 'IMAP un POP serveris nodrošina standarta pakalpojumu komplektu pasta klientiem (Thunderbird, Mail, Evolution, Outlook / Express utt.), Lai izveidotu savienojumu ar serveri un ielādētu / parādītu e-pastu.';
+$lang['imap_app_name'] = 'IMAP un POP serveris';
+$lang['imap_failed_logins'] = 'IMAP pasta kļūdas žurnāli';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Pasta pakalpojums ir nederīgs.';
+$lang['imap_imaps'] = 'Drošs IMAP';
+$lang['imap_mail_service_invalid'] = 'Pasta pakalpojums nav derīgs.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Secure POP';
-$lang['imap_push_email'] = 'Push E-pasts';
+$lang['imap_pop3s'] = 'Droša POP';
+$lang['imap_push_email'] = 'Nospiediet e-pastu';

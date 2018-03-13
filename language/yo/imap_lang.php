a412dba1c395623dad1869e107d7f3af01e15efd
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-13 22:37:03 +0000

    Add new files and directories, commit changes, push changes

--- language/yo/imap_lang.php
+++ language/yo/imap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['imap_app_description'] = 'Awọn IMAP ati POP server pese a boṣewa ṣeto awọn iṣẹ fun mail ibara (Thunderbird, Mail, Evolution, Outlook / Express ati be be lo) lati sopọ si olupin ati gba / àpapọ imeeli.';
+$lang['imap_app_description'] = 'Olupin IMAP ati olupin POP pese apèsè ti o ṣe deede fun awọn onibara mail (Thunderbird, Mail, Evolution, Outlook / Express ati bẹbẹ lọ) lati sopọ si olupin naa ati gba / imeeli ikede.';
 $lang['imap_app_name'] = 'IMAP ati POP Server';
-$lang['imap_failed_logins'] = 'IMAP Mail Kuna logins';
+$lang['imap_failed_logins'] = 'Ifiweranṣẹ IMAP ti kede logins';
 $lang['imap_imap'] = 'IMAP';
-$lang['imap_imaps'] = 'Secure IMAP';
-$lang['imap_mail_service_invalid'] = 'Mail iṣẹ ni invalid.';
+$lang['imap_imaps'] = 'Asiko IMAP';
+$lang['imap_mail_service_invalid'] = 'Iṣẹ i-meeli jẹ aṣiṣe.';
 $lang['imap_pop3'] = 'POP';
-$lang['imap_pop3s'] = 'Secure POP';
+$lang['imap_pop3s'] = 'POP Aladidi';
 $lang['imap_push_email'] = 'Titari E-mail';
